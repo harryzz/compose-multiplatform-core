aosp-mirror/platform_frameworks_support

Create WorkParameters class for initializing Workers.
- WorkParameters is public and not hidden (but a couple of
  methods are hidden for now).
- Remove Extras and refactor parts of it into this class.
- WorkParameters will be used in Worker factory classes and
  internalInit will be deprecated and eventually removed.
- Remove extraneous method in WorkerWrapper.

Test: Updated and ran tests.
Change-Id: I438a6e7e361318803adf07192b11c94622626a1f

work/workmanager/src/main/java/androidx/work/impl/Processor.java
@@ -22,6 +22,7 @@ import android.support.annotation.RestrictTo;
 import androidx.concurrent.listenablefuture.ListenableFuture;
 import androidx.work.Configuration;
 import androidx.work.Logger;
+import androidx.work.WorkParameters;
 import androidx.work.impl.utils.taskexecutor.TaskExecutor;
 
 import java.util.ArrayList;
@@ -84,10 +85,10 @@ public class Processor implements ExecutionListener {
      * Starts a given unit of work in the background.
      *
      * @param id The work id to execute.
-     * @param runtimeExtras The {@link Extras.RuntimeExtras} for this work, if any.
+     * @param runtimeExtras The {@link WorkParameters.RuntimeExtras} for this work, if any.
      * @return {@code true} if the work was successfully enqueued for processing
      */
-    public boolean startWork(String id, Extras.RuntimeExtras runtimeExtras) {
+    public boolean startWork(String id, WorkParameters.RuntimeExtras runtimeExtras) {
         WorkerWrapper workWrapper;
         synchronized (mLock) {
             // Work may get triggered multiple times if they have passing constraints

work/workmanager/src/main/java/androidx/work/impl/background/systemjob/SystemJobService.java
@@ -27,8 +27,8 @@ import android.support.annotation.RestrictTo;
 import android.text.TextUtils;
 
 import androidx.work.Logger;
+import androidx.work.WorkParameters;
 import androidx.work.impl.ExecutionListener;
-import androidx.work.impl.Extras;
 import androidx.work.impl.WorkManagerImpl;
 
 import java.util.HashMap;
@@ -116,9 +116,9 @@ public class SystemJobService extends JobService implements ExecutionListener {
             mJobParameters.put(workSpecId, params);
         }
 
-        Extras.RuntimeExtras runtimeExtras = null;
+        WorkParameters.RuntimeExtras runtimeExtras = null;
         if (Build.VERSION.SDK_INT >= 24) {
-            runtimeExtras = new Extras.RuntimeExtras();
+            runtimeExtras = new WorkParameters.RuntimeExtras();
             if (params.getTriggeredContentUris() != null
                     || params.getTriggeredContentAuthorities() != null) {
                 runtimeExtras.triggeredContentUris = params.getTriggeredContentUris();

work/workmanager/src/main/java/androidx/work/impl/utils/StartWorkRunnable.java
@@ -18,7 +18,7 @@ package androidx.work.impl.utils;
 
 import android.support.annotation.RestrictTo;
 
-import androidx.work.impl.Extras;
+import androidx.work.WorkParameters;
 import androidx.work.impl.WorkManagerImpl;
 
 /**
@@ -31,12 +31,12 @@ public class StartWorkRunnable implements Runnable {
 
     private WorkManagerImpl mWorkManagerImpl;
     private String mWorkSpecId;
-    private Extras.RuntimeExtras mRuntimeExtras;
+    private WorkParameters.RuntimeExtras mRuntimeExtras;
 
     public StartWorkRunnable(
             WorkManagerImpl workManagerImpl,
             String workSpecId,
-            Extras.RuntimeExtras runtimeExtras) {
+            WorkParameters.RuntimeExtras runtimeExtras) {
         mWorkManagerImpl = workManagerImpl;
         mWorkSpecId = workSpecId;
         mRuntimeExtras = runtimeExtras;

harryzz/compose-multiplatform-core

work/workmanager/src/main/java/androidx/work/impl/Processor.java
@@ -21,6 +21,7 @@ import android.support.annotation.RestrictTo;
 
 import androidx.work.Configuration;
 import androidx.work.Logger;
+import androidx.work.WorkerParameters;
 import androidx.work.impl.utils.taskexecutor.TaskExecutor;
 
 import com.google.common.util.concurrent.ListenableFuture;
@@ -85,10 +86,10 @@ public class Processor implements ExecutionListener {
      * Starts a given unit of work in the background.
      *
      * @param id The work id to execute.
-     * @param runtimeExtras The {@link Extras.RuntimeExtras} for this work, if any.
+     * @param runtimeExtras The {@link WorkerParameters.RuntimeExtras} for this work, if any.
      * @return {@code true} if the work was successfully enqueued for processing
      */
-    public boolean startWork(String id, Extras.RuntimeExtras runtimeExtras) {
+    public boolean startWork(String id, WorkerParameters.RuntimeExtras runtimeExtras) {
         WorkerWrapper workWrapper;
         synchronized (mLock) {
             // Work may get triggered multiple times if they have passing constraints

work/workmanager/src/main/java/androidx/work/impl/background/systemjob/SystemJobService.java
@@ -27,8 +27,8 @@ import android.support.annotation.RestrictTo;
 import android.text.TextUtils;
 
 import androidx.work.Logger;
+import androidx.work.WorkerParameters;
 import androidx.work.impl.ExecutionListener;
-import androidx.work.impl.Extras;
 import androidx.work.impl.WorkManagerImpl;
 
 import java.util.HashMap;
@@ -116,9 +116,9 @@ public class SystemJobService extends JobService implements ExecutionListener {
             mJobParameters.put(workSpecId, params);
         }
 
-        Extras.RuntimeExtras runtimeExtras = null;
+        WorkerParameters.RuntimeExtras runtimeExtras = null;
         if (Build.VERSION.SDK_INT >= 24) {
-            runtimeExtras = new Extras.RuntimeExtras();
+            runtimeExtras = new WorkerParameters.RuntimeExtras();
             if (params.getTriggeredContentUris() != null
                     || params.getTriggeredContentAuthorities() != null) {
                 runtimeExtras.triggeredContentUris = params.getTriggeredContentUris();

work/workmanager/src/main/java/androidx/work/impl/utils/StartWorkRunnable.java
@@ -18,7 +18,7 @@ package androidx.work.impl.utils;
 
 import android.support.annotation.RestrictTo;
 
-import androidx.work.impl.Extras;
+import androidx.work.WorkerParameters;
 import androidx.work.impl.WorkManagerImpl;
 
 /**
@@ -31,12 +31,12 @@ public class StartWorkRunnable implements Runnable {
 
     private WorkManagerImpl mWorkManagerImpl;
     private String mWorkSpecId;
-    private Extras.RuntimeExtras mRuntimeExtras;
+    private WorkerParameters.RuntimeExtras mRuntimeExtras;
 
     public StartWorkRunnable(
             WorkManagerImpl workManagerImpl,
             String workSpecId,
-            Extras.RuntimeExtras runtimeExtras) {
+            WorkerParameters.RuntimeExtras runtimeExtras) {
         mWorkManagerImpl = workManagerImpl;
         mWorkSpecId = workSpecId;
         mRuntimeExtras = runtimeExtras;