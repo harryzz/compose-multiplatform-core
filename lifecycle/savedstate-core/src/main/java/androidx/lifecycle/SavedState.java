aosp-mirror/platform_frameworks_support

Expose SavedState as API
Changes:
- SavedStateStore -> SavedState
- Adds SavedState implementation = SavedStateRegistry
- Remove Arguments from SavedState, make them accessible only from
ViewModel specific apis (SavedStateHandle)
- Unhide SavedState & SavedStateRegistry as real apis

Test: ./gradlew :lifecycle:lifecycle-savedstate-core:cC

Change-Id: I4b6ba897b28a5e149e1b1c55a7ea1696c1073a04

lifecycle/savedstate-core/src/main/java/androidx/lifecycle/SavedState.java
@@ -1,0 +1,96 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.lifecycle;
+
+import android.os.Bundle;
+
+import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * A class for managing saved state.
+ */
+public interface SavedState {
+    /**
+     * Consumes saved state previously supplied by {@link SavedState.SaveStateCallback} registered
+     * via {@link #registerSaveStateCallback(String, SaveStateCallback)} with the given {@code key}.
+     * <p>
+     * This call clears an internal reference to returned saved state, so if you call it second time
+     * in the row it will return null.
+     * <p>
+     * All unconsumed values will be saved during {@code onSaveInstanceState(Bundle savedState)}
+     * <p>
+     * This method can be called after {@code super.onCreate(savedStateBundle)} of the corresponding
+     * component. Calling it before that will result in {@code IllegalArgumentException}.
+     * {@link Lifecycle.Event#ON_CREATE} can be used as a signal
+     * that a saved state can be consumed.
+     *
+     * @param key a key with which {@link SavedState.SaveStateCallback} was previously registered.
+     * @return {@code Bundle} with the previously saved state or {@code null}
+     */
+    @MainThread
+    @Nullable
+    Bundle consumeRestoredStateForKey(@NonNull String key);
+
+    /**
+     * Returns if a state was restored and can be safely consumed
+     * with {@link #consumeRestoredStateForKey(String)}
+     *
+     * @return true if state was restored.
+     */
+    @MainThread
+    boolean isRestored();
+
+    /**
+     * This interface marks a component that contributes to saved state.
+     */
+    interface SaveStateCallback {
+        /**
+         * Called to retrieve a state from a component before being killed
+         * so later the state can be received from {@link #consumeRestoredStateForKey(String)}
+         *
+         * @return Bundle with your saved state.
+         */
+        @NonNull
+        Bundle saveState();
+    }
+
+    /**
+     * Registers a {@link SaveStateCallback} by the given {@code key}. This callback will be called
+     * during state saving phase, returned bundle will be associated with the given {@code key}
+     * and can be used after the restoration via {@link #consumeRestoredStateForKey(String)}.
+     *
+     * <p>
+     * If there is unconsumed value with the same {@code key},
+     * the value supplied by {@code callback} will be override and will be written to resulting
+     * saved state bundle.
+     *
+     * @param key a key with which returned saved state will be associated
+     * @param callback callback to get saved state.
+     */
+    @MainThread
+    void registerSaveStateCallback(@NonNull String key, @NonNull SaveStateCallback callback);
+
+    /**
+     * Unregisters a component previously registered by the given {@code key}
+     *
+     * @param key a key with which a component was previously registered.
+     */
+    @MainThread
+    void unregisterSaveStateCallback(@NonNull String key);
+}

lifecycle/savedstate-fragment/src/androidTest/java/androidx/lifecycle/savedstate/SavedStateRegistriesTest.java
@@ -25,15 +25,15 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.os.Bundle;
 
-
 import androidx.annotation.NonNull;
 import androidx.arch.core.util.Function;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.OnLifecycleEvent;
-import androidx.lifecycle.SavedStateStore;
-import androidx.lifecycle.SavedStateStores;
+import androidx.lifecycle.SavedState;
+import androidx.lifecycle.SavedStateRegistries;
+import androidx.lifecycle.SavedStateRegistry;
 import androidx.lifecycle.savedstate.activity.SavedStateActivity;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.filters.MediumTest;
@@ -50,7 +50,7 @@ import java.util.Collection;
 
 @MediumTest
 @RunWith(Parameterized.class)
-public class SavedStateStoresTest {
+public class SavedStateRegistriesTest {
     private static final String KEY = "key";
     private static final String VALUE = "value";
     private static final String FRAGMENT_MODE = "fragment";
@@ -62,11 +62,11 @@ public class SavedStateStoresTest {
         return Arrays.asList(FRAGMENT_MODE, ACTIVITY_MODE);
     }
 
-    private SavedStateStore testedSavedStore(SavedStateActivity currentActivity) {
+    private SavedStateRegistry testedSavedStateRegistry(SavedStateActivity currentActivity) {
         if (FRAGMENT_MODE.equals(mode)) {
-            return SavedStateStores.of(currentActivity.getFragment());
+            return SavedStateRegistries.of(currentActivity.getFragment());
         } else {
-            return SavedStateStores.of(currentActivity);
+            return SavedStateRegistries.of(currentActivity);
         }
     }
 
@@ -99,10 +99,10 @@ public class SavedStateStoresTest {
                 Lifecycle.State currentState = testedLifecycleOwner(activity)
                         .getLifecycle().getCurrentState();
                 assertThat(currentState.isAtLeast(Lifecycle.State.CREATED), is(true));
-                SavedStateStore store = testedSavedStore(activity);
+                SavedStateRegistry store = testedSavedStateRegistry(activity);
                 assertThat(store.consumeRestoredStateForKey(CALLBACK_KEY), nullValue());
-                testedSavedStore(activity)
-                        .registerSavedStateCallback(CALLBACK_KEY, new DefaultSavedStateCallback());
+                testedSavedStateRegistry(activity)
+                        .registerSaveStateCallback(CALLBACK_KEY, new DefaultSavedStateCallback());
             }
         });
         return activity;
@@ -118,7 +118,7 @@ public class SavedStateStoresTest {
                 Lifecycle.State currentState = testedLifecycleOwner(recreated)
                         .getLifecycle().getCurrentState();
                 assertThat(currentState.isAtLeast(Lifecycle.State.CREATED), is(true));
-                checkDefaultSavedState(testedSavedStore(recreated));
+                checkDefaultSavedState(testedSavedStateRegistry(recreated));
             }
         });
     }
@@ -133,7 +133,7 @@ public class SavedStateStoresTest {
                 recreated.getLifecycle().addObserver(new LifecycleObserver() {
                     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
                     void onResume() {
-                        checkDefaultSavedState(testedSavedStore(recreated));
+                        checkDefaultSavedState(testedSavedStateRegistry(recreated));
                     }
                 });
             }
@@ -145,9 +145,9 @@ public class SavedStateStoresTest {
         SavedStateActivity activity = initializeSavedState();
 
         SavedStateActivity.duringOnCreate(FRAGMENT_MODE.equals(mode),
-                new Function<SavedStateStore, Void>() {
+                new Function<SavedStateRegistry, Void>() {
                     @Override
-                    public Void apply(SavedStateStore store) {
+                    public Void apply(SavedStateRegistry store) {
                         checkDefaultSavedState(store);
                         return null;
                     }
@@ -155,17 +155,17 @@ public class SavedStateStoresTest {
         recreateActivity(activity, mActivityRule);
     }
 
-    private static class DefaultSavedStateCallback implements SavedStateStore.SavedStateCallback {
+    private static class DefaultSavedStateCallback implements SavedState.SaveStateCallback {
         @NonNull
         @Override
-        public Bundle getSavedState() {
+        public Bundle saveState() {
             Bundle foo = new Bundle();
             foo.putString(KEY, VALUE);
             return foo;
         }
     }
 
-    private static void checkDefaultSavedState(SavedStateStore store) {
+    private static void checkDefaultSavedState(SavedStateRegistry store) {
         Bundle savedState = store.consumeRestoredStateForKey(CALLBACK_KEY);
         assertThat(savedState, notNullValue());
         //noinspection ConstantConditions

lifecycle/viewmodel-savedstate/src/main/java/androidx/lifecycle/SavedStateAccessorHolder.java
@@ -27,7 +27,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-class SavedStateAccessorHolder implements SavedStateStore.SavedStateCallback {
+class SavedStateAccessorHolder implements SavedStateRegistry.SaveStateCallback {
     private static final String VALUES = "values";
     private static final String KEYS = "keys";
 
@@ -62,7 +62,7 @@ class SavedStateAccessorHolder implements SavedStateStore.SavedStateCallback {
     @SuppressWarnings("unchecked")
     @NonNull
     @Override
-    public Bundle getSavedState() {
+    public Bundle saveState() {
         Set<String> keySet = mAccessor.keys();
         ArrayList keys = new ArrayList(keySet.size());
         ArrayList value = new ArrayList(keys.size());

harryzz/compose-multiplatform-core

lifecycle/savedstate-core/src/main/java/androidx/lifecycle/SavedState.java
@@ -1,0 +1,96 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.lifecycle;
+
+import android.os.Bundle;
+
+import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * A class for managing saved state.
+ */
+public interface SavedState {
+    /**
+     * Consumes saved state previously supplied by {@link SavedState.Callback} registered
+     * via {@link #registerSaveStateCallback(String, Callback)} with the given {@code key}.
+     * <p>
+     * This call clears an internal reference to returned saved state, so if you call it second time
+     * in the row it will return null.
+     * <p>
+     * All unconsumed values will be saved during {@code onSaveInstanceState(Bundle savedState)}
+     * <p>
+     * This method can be called after {@code super.onCreate(savedStateBundle)} of the corresponding
+     * component. Calling it before that will result in {@code IllegalArgumentException}.
+     * {@link Lifecycle.Event#ON_CREATE} can be used as a signal
+     * that a saved state can be consumed.
+     *
+     * @param key a key with which {@link SavedState.Callback} was previously registered.
+     * @return {@code Bundle} with the previously saved state or {@code null}
+     */
+    @MainThread
+    @Nullable
+    Bundle consumeRestoredStateForKey(@NonNull String key);
+
+    /**
+     * Returns if a state was restored and can be safely consumed
+     * with {@link #consumeRestoredStateForKey(String)}
+     *
+     * @return true if state was restored.
+     */
+    @MainThread
+    boolean isRestored();
+
+    /**
+     * This interface marks a component that contributes to saved state.
+     */
+    interface Callback {
+        /**
+         * Called to retrieve a state from a component before being killed
+         * so later the state can be received from {@link #consumeRestoredStateForKey(String)}
+         *
+         * @return Bundle with your saved state.
+         */
+        @NonNull
+        Bundle saveState();
+    }
+
+    /**
+     * Registers a {@link Callback} by the given {@code key}. This callback will be called
+     * during state saving phase, returned bundle will be associated with the given {@code key}
+     * and can be used after the restoration via {@link #consumeRestoredStateForKey(String)}.
+     *
+     * <p>
+     * If there is unconsumed value with the same {@code key},
+     * the value supplied by {@code callback} will be override and will be written to resulting
+     * saved state bundle.
+     *
+     * @param key a key with which returned saved state will be associated
+     * @param callback callback to get saved state.
+     */
+    @MainThread
+    void registerSaveStateCallback(@NonNull String key, @NonNull Callback callback);
+
+    /**
+     * Unregisters a component previously registered by the given {@code key}
+     *
+     * @param key a key with which a component was previously registered.
+     */
+    @MainThread
+    void unregisterSaveStateCallback(@NonNull String key);
+}

lifecycle/savedstate-fragment/src/androidTest/java/androidx/lifecycle/savedstate/SavedStateRegistriesTest.java
@@ -25,15 +25,15 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.os.Bundle;
 
-
 import androidx.annotation.NonNull;
 import androidx.arch.core.util.Function;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.OnLifecycleEvent;
-import androidx.lifecycle.SavedStateStore;
-import androidx.lifecycle.SavedStateStores;
+import androidx.lifecycle.SavedState;
+import androidx.lifecycle.SavedStateRegistries;
+import androidx.lifecycle.SavedStateRegistry;
 import androidx.lifecycle.savedstate.activity.SavedStateActivity;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.filters.MediumTest;
@@ -50,7 +50,7 @@ import java.util.Collection;
 
 @MediumTest
 @RunWith(Parameterized.class)
-public class SavedStateStoresTest {
+public class SavedStateRegistriesTest {
     private static final String KEY = "key";
     private static final String VALUE = "value";
     private static final String FRAGMENT_MODE = "fragment";
@@ -62,11 +62,11 @@ public class SavedStateStoresTest {
         return Arrays.asList(FRAGMENT_MODE, ACTIVITY_MODE);
     }
 
-    private SavedStateStore testedSavedStore(SavedStateActivity currentActivity) {
+    private SavedStateRegistry testedSavedStateRegistry(SavedStateActivity currentActivity) {
         if (FRAGMENT_MODE.equals(mode)) {
-            return SavedStateStores.of(currentActivity.getFragment());
+            return SavedStateRegistries.of(currentActivity.getFragment());
         } else {
-            return SavedStateStores.of(currentActivity);
+            return SavedStateRegistries.of(currentActivity);
         }
     }
 
@@ -99,10 +99,10 @@ public class SavedStateStoresTest {
                 Lifecycle.State currentState = testedLifecycleOwner(activity)
                         .getLifecycle().getCurrentState();
                 assertThat(currentState.isAtLeast(Lifecycle.State.CREATED), is(true));
-                SavedStateStore store = testedSavedStore(activity);
+                SavedStateRegistry store = testedSavedStateRegistry(activity);
                 assertThat(store.consumeRestoredStateForKey(CALLBACK_KEY), nullValue());
-                testedSavedStore(activity)
-                        .registerSavedStateCallback(CALLBACK_KEY, new DefaultSavedStateCallback());
+                testedSavedStateRegistry(activity)
+                        .registerSaveStateCallback(CALLBACK_KEY, new DefaultSavedStateCallback());
             }
         });
         return activity;
@@ -118,7 +118,7 @@ public class SavedStateStoresTest {
                 Lifecycle.State currentState = testedLifecycleOwner(recreated)
                         .getLifecycle().getCurrentState();
                 assertThat(currentState.isAtLeast(Lifecycle.State.CREATED), is(true));
-                checkDefaultSavedState(testedSavedStore(recreated));
+                checkDefaultSavedState(testedSavedStateRegistry(recreated));
             }
         });
     }
@@ -133,7 +133,7 @@ public class SavedStateStoresTest {
                 recreated.getLifecycle().addObserver(new LifecycleObserver() {
                     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
                     void onResume() {
-                        checkDefaultSavedState(testedSavedStore(recreated));
+                        checkDefaultSavedState(testedSavedStateRegistry(recreated));
                     }
                 });
             }
@@ -145,9 +145,9 @@ public class SavedStateStoresTest {
         SavedStateActivity activity = initializeSavedState();
 
         SavedStateActivity.duringOnCreate(FRAGMENT_MODE.equals(mode),
-                new Function<SavedStateStore, Void>() {
+                new Function<SavedStateRegistry, Void>() {
                     @Override
-                    public Void apply(SavedStateStore store) {
+                    public Void apply(SavedStateRegistry store) {
                         checkDefaultSavedState(store);
                         return null;
                     }
@@ -155,17 +155,17 @@ public class SavedStateStoresTest {
         recreateActivity(activity, mActivityRule);
     }
 
-    private static class DefaultSavedStateCallback implements SavedStateStore.SavedStateCallback {
+    private static class DefaultSavedStateCallback implements SavedState.Callback {
         @NonNull
         @Override
-        public Bundle getSavedState() {
+        public Bundle saveState() {
             Bundle foo = new Bundle();
             foo.putString(KEY, VALUE);
             return foo;
         }
     }
 
-    private static void checkDefaultSavedState(SavedStateStore store) {
+    private static void checkDefaultSavedState(SavedStateRegistry store) {
         Bundle savedState = store.consumeRestoredStateForKey(CALLBACK_KEY);
         assertThat(savedState, notNullValue());
         //noinspection ConstantConditions

lifecycle/viewmodel-savedstate/src/main/java/androidx/lifecycle/SavedStateAccessorHolder.java
@@ -27,7 +27,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-class SavedStateAccessorHolder implements SavedStateStore.SavedStateCallback {
+class SavedStateAccessorHolder implements SavedState.Callback {
     private static final String VALUES = "values";
     private static final String KEYS = "keys";
 
@@ -62,7 +62,7 @@ class SavedStateAccessorHolder implements SavedStateStore.SavedStateCallback {
     @SuppressWarnings("unchecked")
     @NonNull
     @Override
-    public Bundle getSavedState() {
+    public Bundle saveState() {
         Set<String> keySet = mAccessor.keys();
         ArrayList keys = new ArrayList(keySet.size());
         ArrayList value = new ArrayList(keys.size());