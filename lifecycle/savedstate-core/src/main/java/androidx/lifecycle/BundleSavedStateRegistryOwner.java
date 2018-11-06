aosp-mirror/platform_frameworks_support

Generify SavedStateRegistry instead of using Bundle explicitly.
It opens two major possibilities:
1. to have a SavedStateRegistry with PersistableBundle
2. to have a custom not android specific
   SavedStateRegistry implementation

bug: 117485891
Test: ./gradlew :lifecycle:lifecycle-savedstate-fragment:cC
Change-Id: I4fa81a358096dff874b82246e4e97bff33944fef

lifecycle/savedstate-core/src/main/java/androidx/lifecycle/BundleSavedStateRegistryOwner.java
@@ -17,17 +17,19 @@
 package androidx.lifecycle;
 
 
+import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 
 /**
  * A scope that owns {@link SavedStateRegistry}
  */
-public interface SavedStateRegistryOwner {
+public interface BundleSavedStateRegistryOwner {
     /**
      * Returns owned {@link SavedStateRegistry}
      *
      * @return a {@link SavedStateRegistry}
      */
     @NonNull
-    SavedStateRegistry getSavedState();
+    SavedStateRegistry<Bundle> getSavedState();
 }

harryzz/compose-multiplatform-core

lifecycle/savedstate-core/src/main/java/androidx/lifecycle/BundleSavedStateRegistryOwner.java
@@ -17,17 +17,19 @@
 package androidx.lifecycle;
 
 
+import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 
 /**
  * A scope that owns {@link SavedStateRegistry}
  */
-public interface SavedStateRegistryOwner {
+public interface BundleSavedStateRegistryOwner {
     /**
      * Returns owned {@link SavedStateRegistry}
      *
      * @return a {@link SavedStateRegistry}
      */
     @NonNull
-    SavedStateRegistry getSavedState();
+    SavedStateRegistry<Bundle> getBundleSavedStateRegistry();
 }