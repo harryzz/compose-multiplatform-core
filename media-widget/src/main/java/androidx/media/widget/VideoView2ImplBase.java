aosp-mirror/platform_frameworks_support

MediaPlayer: use a single structure for querying video size.
Test: ./gradlew media2/media-widget:connectedCheck
Bug: 118902787
Change-Id: I26f75593cd2c719dc2c3df8aa31c967e9875289a

media-widget/src/main/java/androidx/media/widget/VideoView2ImplBase.java
@@ -60,6 +60,7 @@ import androidx.media2.SessionPlayer2;
 import androidx.media2.SessionToken2;
 import androidx.media2.SubtitleData2;
 import androidx.media2.UriMediaItem2;
+import androidx.media2.VideoSize;
 import androidx.media2.subtitle.Cea708CaptionRenderer;
 import androidx.media2.subtitle.ClosedCaptionRenderer;
 import androidx.media2.subtitle.SubtitleController;
@@ -898,9 +899,10 @@ class VideoView2ImplBase implements VideoView2Impl, VideoViewInterface.SurfaceLi
             new MediaPlayer.PlayerCallback() {
                 @Override
                 public void onVideoSizeChanged(
-                        MediaPlayer mp, MediaItem2 dsd, int width, int height) {
+                        MediaPlayer mp, MediaItem2 dsd, VideoSize size) {
                     if (DEBUG) {
-                        Log.d(TAG, "onVideoSizeChanged(): size: " + width + "/" + height);
+                        Log.d(TAG, "onVideoSizeChanged(): size: " + size.getWidth() + "/"
+                                + size.getHeight());
                     }
                     if (mp != mMediaPlayer) {
                         if (DEBUG) {
@@ -908,8 +910,8 @@ class VideoView2ImplBase implements VideoView2Impl, VideoViewInterface.SurfaceLi
                         }
                         return;
                     }
-                    mVideoWidth = width;
-                    mVideoHeight = height;
+                    mVideoWidth = size.getWidth();
+                    mVideoHeight = size.getHeight();
                     if (DEBUG) {
                         Log.d(TAG, "onVideoSizeChanged(): mVideoSize:" + mVideoWidth + "/"
                                 + mVideoHeight);
@@ -1054,8 +1056,9 @@ class VideoView2ImplBase implements VideoView2Impl, VideoViewInterface.SurfaceLi
                     }
 
                     if (player instanceof VideoView2Player) {
-                        int videoWidth = ((VideoView2Player) player).getVideoWidth();
-                        int videoHeight = ((VideoView2Player) player).getVideoHeight();
+                        VideoSize size = ((VideoView2Player) player).getVideoSize();
+                        int videoWidth = size.getWidth();
+                        int videoHeight = size.getHeight();
 
                         if (videoWidth != 0 && videoHeight != 0) {
                             if (videoWidth != mVideoWidth || videoHeight != mVideoHeight) {

media2/src/androidTest/java/androidx/media2/MediaPlayerDrmTest.java
@@ -379,8 +379,9 @@ public class MediaPlayerDrmTest {
 
         mECb = new MediaPlayer.PlayerCallback() {
                 @Override
-                public void onVideoSizeChanged(MediaPlayer mp, MediaItem2 item, int w, int h) {
-                    Log.v(TAG, "VideoSizeChanged" + " w:" + w + " h:" + h);
+                public void onVideoSizeChanged(MediaPlayer mp, MediaItem2 item, VideoSize size) {
+                    Log.v(TAG, "VideoSizeChanged" + " w:" + size.getWidth() + " h:"
+                            + size.getHeight());
                     mOnVideoSizeChangedCalled.signal();
                 }
 

media2/src/androidTest/java/androidx/media2/MediaPlayerTest.java
@@ -180,15 +180,15 @@ public class MediaPlayerTest extends MediaPlayerTestBase {
         final TestUtils.Monitor onVideoRenderingStartCalled = new TestUtils.Monitor();
         MediaPlayer.PlayerCallback callback = new MediaPlayer.PlayerCallback() {
             @Override
-            public void onVideoSizeChanged(MediaPlayer mp, MediaItem2 dsd, int w, int h) {
-                if (w == 0 && h == 0) {
+            public void onVideoSizeChanged(MediaPlayer mp, MediaItem2 dsd, VideoSize size) {
+                if (size.getWidth() == 0 && size.getHeight() == 0) {
                     // A size of 0x0 can be sent initially one time when using NuPlayer.
                     assertFalse(onVideoSizeChangedCalled.isSignalled());
                     return;
                 }
                 onVideoSizeChangedCalled.signal();
-                assertEquals(width, w);
-                assertEquals(height, h);
+                assertEquals(width, size.getWidth());
+                assertEquals(height, size.getHeight());
             }
 
             @Override

harryzz/compose-multiplatform-core

media-widget/src/main/java/androidx/media/widget/VideoView2ImplBase.java
@@ -60,6 +60,7 @@ import androidx.media2.SessionPlayer;
 import androidx.media2.SessionToken;
 import androidx.media2.SubtitleData;
 import androidx.media2.UriMediaItem;
+import androidx.media2.VideoSize;
 import androidx.media2.subtitle.Cea708CaptionRenderer;
 import androidx.media2.subtitle.ClosedCaptionRenderer;
 import androidx.media2.subtitle.SubtitleController;
@@ -898,9 +899,10 @@ class VideoView2ImplBase implements VideoView2Impl, VideoViewInterface.SurfaceLi
             new MediaPlayer.PlayerCallback() {
                 @Override
                 public void onVideoSizeChanged(
-                        MediaPlayer mp, MediaItem dsd, int width, int height) {
+                        MediaPlayer mp, MediaItem dsd, VideoSize size) {
                     if (DEBUG) {
-                        Log.d(TAG, "onVideoSizeChanged(): size: " + width + "/" + height);
+                        Log.d(TAG, "onVideoSizeChanged(): size: " + size.getWidth() + "/"
+                                + size.getHeight());
                     }
                     if (mp != mMediaPlayer) {
                         if (DEBUG) {
@@ -908,8 +910,8 @@ class VideoView2ImplBase implements VideoView2Impl, VideoViewInterface.SurfaceLi
                         }
                         return;
                     }
-                    mVideoWidth = width;
-                    mVideoHeight = height;
+                    mVideoWidth = size.getWidth();
+                    mVideoHeight = size.getHeight();
                     if (DEBUG) {
                         Log.d(TAG, "onVideoSizeChanged(): mVideoSize:" + mVideoWidth + "/"
                                 + mVideoHeight);
@@ -1054,8 +1056,9 @@ class VideoView2ImplBase implements VideoView2Impl, VideoViewInterface.SurfaceLi
                     }
 
                     if (player instanceof VideoView2Player) {
-                        int videoWidth = ((VideoView2Player) player).getVideoWidth();
-                        int videoHeight = ((VideoView2Player) player).getVideoHeight();
+                        VideoSize size = ((VideoView2Player) player).getVideoSize();
+                        int videoWidth = size.getWidth();
+                        int videoHeight = size.getHeight();
 
                         if (videoWidth != 0 && videoHeight != 0) {
                             if (videoWidth != mVideoWidth || videoHeight != mVideoHeight) {

media2/src/androidTest/java/androidx/media2/MediaPlayerDrmTest.java
@@ -379,8 +379,9 @@ public class MediaPlayerDrmTest {
 
         mECb = new MediaPlayer.PlayerCallback() {
                 @Override
-                public void onVideoSizeChanged(MediaPlayer mp, MediaItem item, int w, int h) {
-                    Log.v(TAG, "VideoSizeChanged" + " w:" + w + " h:" + h);
+                public void onVideoSizeChanged(MediaPlayer mp, MediaItem item, VideoSize size) {
+                    Log.v(TAG, "VideoSizeChanged" + " w:" + size.getWidth() + " h:"
+                            + size.getHeight());
                     mOnVideoSizeChangedCalled.signal();
                 }
 

media2/src/androidTest/java/androidx/media2/MediaPlayerTest.java
@@ -180,15 +180,15 @@ public class MediaPlayerTest extends MediaPlayerTestBase {
         final TestUtils.Monitor onVideoRenderingStartCalled = new TestUtils.Monitor();
         MediaPlayer.PlayerCallback callback = new MediaPlayer.PlayerCallback() {
             @Override
-            public void onVideoSizeChanged(MediaPlayer mp, MediaItem dsd, int w, int h) {
-                if (w == 0 && h == 0) {
+            public void onVideoSizeChanged(MediaPlayer mp, MediaItem dsd, VideoSize size) {
+                if (size.getWidth() == 0 && size.getHeight() == 0) {
                     // A size of 0x0 can be sent initially one time when using NuPlayer.
                     assertFalse(onVideoSizeChangedCalled.isSignalled());
                     return;
                 }
                 onVideoSizeChangedCalled.signal();
-                assertEquals(width, w);
-                assertEquals(height, h);
+                assertEquals(width, size.getWidth());
+                assertEquals(height, size.getHeight());
             }
 
             @Override