aosp-mirror/platform_frameworks_support

Extended VP2 demo with notifyDataSetChanged()
Shows how to use ViewPager2 with a collection that changes its content.

Bug: 109732910
Test: manual

Change-Id: I93eebd30a5a4def44f4db1f5ef73012e72e14b16

samples/ViewPager2Demos/src/main/java/com/example/androidx/viewpager2/BrowseActivity.kt
@@ -39,10 +39,14 @@ class BrowseActivity : ListActivity() {
     private fun getData(): List<Map<String, Any>> {
         val myData = mutableListOf<Map<String, Any>>()
 
-        myData.add(hashMapOf("title" to "ViewPager with Views",
+        myData.add(hashMapOf("title" to "ViewPager2 with Views",
                 "intent" to activityToIntent(CardViewActivity::class.java.name)))
-        myData.add(hashMapOf("title" to "ViewPager with Fragments",
+        myData.add(hashMapOf("title" to "ViewPager2 with Fragments",
                 "intent" to activityToIntent(CardFragmentActivity::class.java.name)))
+        myData.add(hashMapOf("title" to "ViewPager2 with a Mutable Collection (Views)",
+                "intent" to activityToIntent(MutableCollectionViewActivity::class.java.name)))
+        myData.add(hashMapOf("title" to "ViewPager2 with a Mutable Collection (Fragments)",
+                "intent" to activityToIntent(MutableCollectionFragmentActivity::class.java.name)))
 
         return myData
     }

harryzz/compose-multiplatform-core

samples/ViewPager2Demos/src/main/java/com/example/androidx/viewpager2/BrowseActivity.kt
@@ -39,10 +39,14 @@ class BrowseActivity : ListActivity() {
     private fun getData(): List<Map<String, Any>> {
         val myData = mutableListOf<Map<String, Any>>()
 
-        myData.add(hashMapOf("title" to "ViewPager with Views",
+        myData.add(mapOf("title" to "ViewPager2 with Views",
                 "intent" to activityToIntent(CardViewActivity::class.java.name)))
-        myData.add(hashMapOf("title" to "ViewPager with Fragments",
+        myData.add(mapOf("title" to "ViewPager2 with Fragments",
                 "intent" to activityToIntent(CardFragmentActivity::class.java.name)))
+        myData.add(mapOf("title" to "ViewPager2 with a Mutable Collection (Views)",
+                "intent" to activityToIntent(MutableCollectionViewActivity::class.java.name)))
+        myData.add(mapOf("title" to "ViewPager2 with a Mutable Collection (Fragments)",
+                "intent" to activityToIntent(MutableCollectionFragmentActivity::class.java.name)))
 
         return myData
     }