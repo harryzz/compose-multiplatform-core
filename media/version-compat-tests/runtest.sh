harryzz/compose-multiplatform-core

commit 47723e11763e6f9476c14ba6f88d825d4f792a07

Fixed broken min/max testing
Lazy evaluation caused maxDepVersions tasks to not switch
dependencies due to module:project map not being complete.

Due to test application ids being similar for all flavors
(apparently an AGP bug), minDepVersions and maxDepVersions
tests could not be ran simultaneously on a device, which is
also a problem.

The following CL forces evaluation of all children before
dependency substitution and substitutes for the debug
variant (cannot substitute for release variant after
evaluation.)

Removed flavors completely to make the build faster since
it makes more sense to test binary compatibility during
presubmits/postsubmits. We now use a flag
(-PuseMaxDepVersions) instead which triggers substitution.
Only debug configurations/tasks are substituted for, which
is fine since buildOnServer, buildTestApks build using
debug tasks. Lint also runs on the debug variant so this
works for our purposes (although it is currently disabled
for maxdepversions as I am investigating some behavior.)

The plan is to run presubmit and postsubmit targets twice
with every second target running with the -PuseMaxDepVersions
flag. This parallelizes runs and guarantees binary compatibility
testing while still running in theoratically the same time as
before flavors (although we are likely to use more resources.)

Test: ./gradlew <project>:dependencies -PuseMaxDepVersions

Observe the dependencies change to project dependencies in debug
configurations. (compared to a run without the flag.)
Also deprecate a method in ToT and make sure that using the flag
on a project depending on a released artifact of the project
containing the deprecated class show a warning about using
deprecated class meaning it did indeed switch to the ToT version
of that dependency. (introducing an error is also another method
of testing, although I personally have not tried it this time.)

Change-Id: Ic9c7a3c5844cda20f1d7e0acd6cf856b6fe035c1

media/version-compat-tests/runtest.sh
@@ -25,7 +25,6 @@
 
 CLIENT_MODULE_NAME_BASE="support-media-compat-test-client"
 SERVICE_MODULE_NAME_BASE="support-media-compat-test-service"
-TEST_VERSIONS=("MaxDepVersions" "MinDepVersions")
 CLIENT_VERSION=""
 SERVICE_VERSION=""
 OPTION_TEST_TARGET=""
@@ -48,28 +47,21 @@ function runTest() {
 
   local CLIENT_MODULE_NAME="$CLIENT_MODULE_NAME_BASE$([ "$CLIENT_VERSION" = "tot" ] || echo "-previous")"
   local SERVICE_MODULE_NAME="$SERVICE_MODULE_NAME_BASE$([ "$SERVICE_VERSION" = "tot" ] || echo "-previous")"
-  for version in "${TEST_VERSIONS[@]}"; do
-    local TEST_TASK_NAME="assemble${version}DebugAndroidTest"
 
     # Build test apks
-    ./gradlew $CLIENT_MODULE_NAME:$TEST_TASK_NAME || { echo "Build failed. Aborting."; exit 1; }
-    ./gradlew $SERVICE_MODULE_NAME:$TEST_TASK_NAME || { echo "Build failed. Aborting."; exit 1; }
+    ./gradlew $CLIENT_MODULE_NAME:assembleDebugAndroidTest || { echo "Build failed. Aborting."; exit 1; }
+    ./gradlew $SERVICE_MODULE_NAME:assembleDebugAndroidTest || { echo "Build failed. Aborting."; exit 1; }
 
-    # Search for the apks.
-    # Need to search under out/host instead of out/dist, because MaxDepVersions doesn't drop apks there.
-    local CLIENT_APK=$(find ../../out/host -iname "$CLIENT_MODULE_NAME-$version-debug-androidTest.apk")
-    local SERVICE_APK=$(find ../../out/host -iname "$SERVICE_MODULE_NAME-$version-debug-androidTest.apk")
-
-    # Install the apks
-    adb install -r $CLIENT_APK || { echo "Failed to install $CLIENT_APK. Aborting."; exit 1; }
-    adb install -r $SERVICE_APK || { echo "Failed to install $SERVICE_APK. Aborting."; exit 1; }
+    ## Install the apks
+    adb install -r "../../out/dist/$CLIENT_MODULE_NAME.apk" || { echo "Apk installation failed. Aborting."; exit 1; }
+    adb install -r "../../out/dist/$SERVICE_MODULE_NAME.apk" || { echo "Apk installation failed. Aborting."; exit 1; }
 
     # Run the tests
     local test_command="adb shell am instrument -w -e debug false -e client_version $CLIENT_VERSION -e service_version $SERVICE_VERSION"
     local client_test_runner="android.support.mediacompat.client.test/androidx.test.runner.AndroidJUnitRunner"
     local service_test_runner="android.support.mediacompat.service.test/androidx.test.runner.AndroidJUnitRunner"
 
-    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Started: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION (Version=$version) <<<<<<<<<<<<<<<<<<<<<<<<"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Started: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<"
 
     if [[ $OPTION_TEST_TARGET == *"client"* ]]; then
       ${test_command} $OPTION_TEST_TARGET ${client_test_runner}
@@ -80,7 +72,7 @@ function runTest() {
       ${test_command} ${service_test_runner}
     fi
 
-    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Ended: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION (Version=$version) <<<<<<<<<<<<<<<<<<<<<<<<<<"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Ended: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<<<"
   done
 }
 

media2/version-compat-tests/runtest.sh
@@ -25,7 +25,6 @@
 
 CLIENT_MODULE_NAME_BASE="support-media2-test-client"
 SERVICE_MODULE_NAME_BASE="support-media2-test-service"
-TEST_VERSIONS=("MaxDepVersions" "MinDepVersions")
 CLIENT_VERSION=""
 SERVICE_VERSION=""
 OPTION_TEST_TARGET=""
@@ -49,28 +48,21 @@ function runTest() {
 
   local CLIENT_MODULE_NAME="$CLIENT_MODULE_NAME_BASE$([ "$CLIENT_VERSION" = "tot" ] || echo "-previous")"
   local SERVICE_MODULE_NAME="$SERVICE_MODULE_NAME_BASE$([ "$SERVICE_VERSION" = "tot" ] || echo "-previous")"
-  for version in "${TEST_VERSIONS[@]}"; do
-    local TEST_TASK_NAME="assemble${version}DebugAndroidTest"
 
-    # Build test apks
-    ./gradlew $CLIENT_MODULE_NAME:$TEST_TASK_NAME || { echo "Build failed. Aborting."; exit 1; }
-    ./gradlew $SERVICE_MODULE_NAME:$TEST_TASK_NAME || { echo "Build failed. Aborting."; exit 1; }
+  # Build test apks
+  ./gradlew $CLIENT_MODULE_NAME:assembleDebugAndroidTest || { echo "Build failed. Aborting."; exit 1; }
+  ./gradlew $SERVICE_MODULE_NAME:assembleDebugAndroidTest || { echo "Build failed. Aborting."; exit 1; }
 
-    # Search for the apks.
-    # Need to search under out/host instead of out/dist, because MaxDepVersions doesn't drop apks there.
-    local CLIENT_APK=$(find ../../out/host -iname "$CLIENT_MODULE_NAME-$version-debug-androidTest.apk")
-    local SERVICE_APK=$(find ../../out/host -iname "$SERVICE_MODULE_NAME-$version-debug-androidTest.apk")
-
-    # Install the apks
-    adb install -r $CLIENT_APK || { echo "Failed to install $CLIENT_APK. Aborting."; exit 1; }
-    adb install -r $SERVICE_APK || { echo "Failed to install $SERVICE_APK. Aborting."; exit 1; }
+  # Install the apks
+  adb install -r "../../out/dist/$CLIENT_MODULE_NAME.apk" || { echo "Apk installation failed. Aborting."; exit 1; }
+  adb install -r "../../out/dist/$SERVICE_MODULE_NAME.apk" || { echo "Apk installation failed. Aborting."; exit 1; }
 
     # Run the tests
     local test_command="adb shell am instrument -w -e debug false -e client_version $CLIENT_VERSION -e service_version $SERVICE_VERSION"
     local client_test_runner="androidx.media2.test.client.test/androidx.test.runner.AndroidJUnitRunner"
     local service_test_runner="androidx.media2.test.service.test/androidx.test.runner.AndroidJUnitRunner"
 
-    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Started: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION (Version=$version) <<<<<<<<<<<<<<<<<<<<<<<<"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Started: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<"
 
     if [[ $OPTION_TEST_TARGET == *"client"* ]]; then
       ${test_command} $OPTION_TEST_TARGET ${client_test_runner}
@@ -81,7 +73,7 @@ function runTest() {
         ${test_command} ${service_test_runner}
     fi
 
-    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Ended: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION (Version=$version) <<<<<<<<<<<<<<<<<<<<<<<<<<"
+    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Ended: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<<<"
   done
 }
 

buildSrc/init.gradle
@@ -93,10 +93,8 @@ def setupSubprojectsForBuildServer() {
         // Copy instrumentation test APKs and app APKs into the dist dir
         // For test apks, they are uploaded only if we have java test sources.
         // For regular app apks, they are uploaded only if they have java sources.
-        // Copy only the minDepVersions debug version since that is the publish
-        // flavor.
         project.tasks.whenTaskAdded { task ->
-            if (task.name.startsWith("packageMinDepVersionsDebug")) {
+            if (task.name.startsWith("packageDebug")) {
                 // run this task only if we should run it
                 def testApk = task.name.contains("AndroidTest")
                 if (testApk) {
@@ -128,10 +126,7 @@ def setupSubprojectsForBuildServer() {
                                 // existing support library presubmit tests.
                                 if (fileName.contains("media-compat-test")
                                         || fileName.contains("media2-test")) {
-                                    fileName
-                                            .replace(fileName,
-                                            "minDepVersions-${fileName}")
-                                            .replace("-minDepVersions-debug-androidTest", "")
+                                    fileName.replace("-debug-androidTest", "")
                                 } else {
                                     // multiple modules may have the same name so prefix the name with
                                     // the module's path to ensure it is unique.

buildSrc/src/main/kotlin/androidx/build/AndroidXPlugin.kt
@@ -148,7 +148,8 @@ class AndroidXPlugin : Plugin<Project> {
                 if ("assembleAndroidTest" == task.name ||
                         "assembleDebug" == task.name ||
                         ERROR_PRONE_TASK == task.name ||
-                        "lintMinDepVersionsDebug" == task.name) {
+                        ("lintDebug" == task.name &&
+                        !project.rootProject.hasProperty("useMaxDepVersions"))) {
                     buildOnServerTask.dependsOn(task)
                 }
                 if ("assembleAndroidTest" == task.name ||
@@ -175,15 +176,23 @@ class AndroidXPlugin : Plugin<Project> {
 
         AffectedModuleDetector.configure(gradle, this)
 
-        // Iterate through all the project and substitute any artifact dependency of a
-        // maxdepversions future configuration with the corresponding tip of tree project.
-        subprojects { project ->
-            project.configurations.all { configuration ->
-                if (configuration.name.toLowerCase().contains("maxdepversions") &&
-                        project.extra.has("publish")) {
-                    configuration.resolutionStrategy.dependencySubstitution.apply {
-                        for (e in projectModules) {
-                            substitute(module(e.key)).with(project(e.value))
+        // If useMaxDepVersions is set, iterate through all the project and substitute any androidx
+        // artifact dependency with the local tip of tree version of the library.
+        if (project.hasProperty("useMaxDepVersions")) {
+            // This requires evaluating all sub-projects to create the module:project map
+            // and project dependencies.
+            evaluationDependsOnChildren()
+            subprojects { project ->
+                project.configurations.all { configuration ->
+                    // Substitute only for debug configurations/tasks only because we can not
+                    // change release dependencies after evaluation. Test hooks, buildOnServer
+                    // and buildTestApks use the debug configurations as well.
+                    if (project.extra.has("publish") && configuration.name
+                            .toLowerCase().contains("debug")) {
+                        configuration.resolutionStrategy.dependencySubstitution.apply {
+                            for (e in projectModules) {
+                                substitute(module(e.key)).with(project(e.value))
+                            }
                         }
                     }
                 }
@@ -219,22 +228,6 @@ class AndroidXPlugin : Plugin<Project> {
                                         "dependency to list a fixed version.")
                     }
                 }
-            }
-        }
-        if (project.name != "docs-fake") {
-            // Add another "version" flavor dimension which would have two flavors minDepVersions
-            // and maxDepVersions. Flavor minDepVersions builds the libraries against the specified
-            // versions of their dependencies while maxDepVersions builds the libraries against
-            // the local versions of their dependencies (so for example if library A specifies
-            // androidx.collection:collection:1.2.0 as its dependency then minDepVersions would
-            // build using exactly that version while maxDepVersions would build against
-            // project(":collection") instead.)
-            extension.flavorDimensions("version")
-            extension.productFlavors {
-                it.create("minDepVersions")
-                it.get("minDepVersions").dimension = "version"
-                it.create("maxDepVersions")
-                it.get("maxDepVersions").dimension = "version"
             }
         }
 
@@ -372,6 +365,6 @@ private fun Project.configureResourceApiChecks() {
 }
 
 private fun Project.getGenerateResourceApiFile(): File {
-    return File(project.buildDir, "intermediates/public_res/minDepVersionsRelease" +
-            "/packageMinDepVersionsReleaseResources/public.txt")
+    return File(project.buildDir, "intermediates/public_res/release" +
+            "/packageReleaseResources/public.txt")
 }

buildSrc/src/main/kotlin/androidx/build/Release.kt
@@ -174,7 +174,7 @@ object Release {
     @Suppress("MemberVisibilityCanBePrivate")
     const val DIFF_TASK_PREFIX = "createDiffArchive"
     const val FULL_ARCHIVE_TASK_NAME = "createArchive"
-    const val DEFAULT_PUBLISH_CONFIG = "minDepVersionsRelease"
+    const val DEFAULT_PUBLISH_CONFIG = "release"
     // lazily created config action params so that we don't keep re-creating them
     private var configActionParams: GMavenZipTask.ConfigAction.Params? = null
 

buildSrc/src/main/kotlin/androidx/build/SupportAndroidLibraryPlugin.kt
@@ -88,10 +88,10 @@ class SupportAndroidLibraryPlugin : Plugin<Project> {
                     if (supportLibraryExtension.failOnDeprecationWarnings) {
                         javaCompile.options.compilerArgs.add("-Xlint:deprecation")
                     }
-                    // We don't want maxDepVersions to fail when it finds a warning because
+                    // We don't want useMaxDepVersions to fail when it finds a warning because
                     // if we introduce warnings to new libraries that were okay to use before,
                     // we'd like to only display a warning and fix later.
-                    if (libraryVariant.flavorName != "maxDepVersions") {
+                    if (!project.rootProject.hasProperty("useMaxDepVersions")) {
                         javaCompile.options.compilerArgs.add("-Werror")
                     }
                 }
@@ -142,9 +142,7 @@ private fun Project.injectCompilationForBenchmarks(
         // only run aotCompile on N+, where it's supported
         val inject = "if ((`getprop ro.build.version.sdk` >= 24)); then $aotCompile; fi"
         // NOTE: we assume here that all benchmarks have apk name $projectname-debug-androidTest.apk
-        // This will only run benchmark generated by minDepVersions builds.
-        // TODO(obenabde): test both build flavors instead of only minDepVersions
-        val options = "/data/local/tmp/$name-minDepVersions-debug-androidTest.apk && $inject #"
+        val options = "/data/local/tmp/$name-debug-androidTest.apk && $inject #"
         extension.adbOptions.setInstallOptions(*options.split(" ").toTypedArray())
     }
 }

navigation/safe-args-generator/build.gradle
@@ -54,7 +54,7 @@ tasks.findByName("test").doFirst {
     it.classpath = files(classpath.minus(androidJar).plus(androidJar))
 }
 
-tasks.findByName("compileKotlin").dependsOn(":navigation:navigation-common:jarMinDepVersionsDebug")
+tasks.findByName("compileKotlin").dependsOn(":navigation:navigation-common:jarDebug")
 
 supportLibrary {
     name = 'Android Navigation TypeSafe Arguments Generator'

room/compiler/build.gradle
@@ -79,8 +79,8 @@ def generateAntlrTask = task('generateAntlrGrammar', type: JavaExec) {
 }
 
 tasks.findByName("compileKotlin").dependsOn(generateAntlrTask)
-tasks.findByName("compileKotlin").dependsOn(":room:room-runtime:jarMinDepVersionsDebug")
-tasks.findByName("compileKotlin").dependsOn(":sqlite:sqlite:jarMinDepVersionsDebug")
+tasks.findByName("compileKotlin").dependsOn(":room:room-runtime:jarDebug")
+tasks.findByName("compileKotlin").dependsOn(":sqlite:sqlite:jarDebug")
 
 supportLibrary {
     name = "Android Room Compiler"

commit a2ac45d45ee1c1b6333ddc7d4bc4f9c34ca6ec38

Fix broken test script
This CL fixes the test script breakage introduced by
"47723e1: Fixed broken min/max testing".

Test: Ran each test script
Change-Id: I0b00cd3771cb710a2b1c589ba0857d2b0d35c8da

media/version-compat-tests/runtest.sh
@@ -48,32 +48,31 @@ function runTest() {
   local CLIENT_MODULE_NAME="$CLIENT_MODULE_NAME_BASE$([ "$CLIENT_VERSION" = "tot" ] || echo "-previous")"
   local SERVICE_MODULE_NAME="$SERVICE_MODULE_NAME_BASE$([ "$SERVICE_VERSION" = "tot" ] || echo "-previous")"
 
-    # Build test apks
-    ./gradlew $CLIENT_MODULE_NAME:assembleDebugAndroidTest || { echo "Build failed. Aborting."; exit 1; }
-    ./gradlew $SERVICE_MODULE_NAME:assembleDebugAndroidTest || { echo "Build failed. Aborting."; exit 1; }
+  echo "Building the test apks"
+  ./gradlew $CLIENT_MODULE_NAME:assembleDebugAndroidTest || { echo "Build failed. Aborting."; exit 1; }
+  ./gradlew $SERVICE_MODULE_NAME:assembleDebugAndroidTest || { echo "Build failed. Aborting."; exit 1; }
 
-    ## Install the apks
-    adb install -r "../../out/dist/$CLIENT_MODULE_NAME.apk" || { echo "Apk installation failed. Aborting."; exit 1; }
-    adb install -r "../../out/dist/$SERVICE_MODULE_NAME.apk" || { echo "Apk installation failed. Aborting."; exit 1; }
+  echo "Installing the test apks"
+  adb install -r "../../out/dist/$CLIENT_MODULE_NAME.apk" || { echo "Apk installation failed. Aborting."; exit 1; }
+  adb install -r "../../out/dist/$SERVICE_MODULE_NAME.apk" || { echo "Apk installation failed. Aborting."; exit 1; }
 
-    # Run the tests
-    local test_command="adb shell am instrument -w -e debug false -e client_version $CLIENT_VERSION -e service_version $SERVICE_VERSION"
-    local client_test_runner="android.support.mediacompat.client.test/androidx.test.runner.AndroidJUnitRunner"
-    local service_test_runner="android.support.mediacompat.service.test/androidx.test.runner.AndroidJUnitRunner"
+  echo "Running the tests"
+  local test_command="adb shell am instrument -w -e debug false -e client_version $CLIENT_VERSION -e service_version $SERVICE_VERSION"
+  local client_test_runner="android.support.mediacompat.client.test/androidx.test.runner.AndroidJUnitRunner"
+  local service_test_runner="android.support.mediacompat.service.test/androidx.test.runner.AndroidJUnitRunner"
 
-    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Started: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<"
+  echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Started: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<"
 
-    if [[ $OPTION_TEST_TARGET == *"client"* ]]; then
-      ${test_command} $OPTION_TEST_TARGET ${client_test_runner}
-    elif [[ $OPTION_TEST_TARGET == *"service"* ]]; then
-      ${test_command} $OPTION_TEST_TARGET ${service_test_runner}
-    else
-      ${test_command} ${client_test_runner}
-      ${test_command} ${service_test_runner}
-    fi
+  if [[ $OPTION_TEST_TARGET == *"client"* ]]; then
+    ${test_command} $OPTION_TEST_TARGET ${client_test_runner}
+  elif [[ $OPTION_TEST_TARGET == *"service"* ]]; then
+    ${test_command} $OPTION_TEST_TARGET ${service_test_runner}
+  else
+    ${test_command} ${client_test_runner}
+    ${test_command} ${service_test_runner}
+  fi
 
-    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Ended: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<<<"
-  done
+  echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Ended: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<<<"
 }
 
 

media2/version-compat-tests/runtest.sh
@@ -49,32 +49,31 @@ function runTest() {
   local CLIENT_MODULE_NAME="$CLIENT_MODULE_NAME_BASE$([ "$CLIENT_VERSION" = "tot" ] || echo "-previous")"
   local SERVICE_MODULE_NAME="$SERVICE_MODULE_NAME_BASE$([ "$SERVICE_VERSION" = "tot" ] || echo "-previous")"
 
-  # Build test apks
+  echo "Building the test apks"
   ./gradlew $CLIENT_MODULE_NAME:assembleDebugAndroidTest || { echo "Build failed. Aborting."; exit 1; }
   ./gradlew $SERVICE_MODULE_NAME:assembleDebugAndroidTest || { echo "Build failed. Aborting."; exit 1; }
 
-  # Install the apks
+  echo "Installing the test apks"
   adb install -r "../../out/dist/$CLIENT_MODULE_NAME.apk" || { echo "Apk installation failed. Aborting."; exit 1; }
   adb install -r "../../out/dist/$SERVICE_MODULE_NAME.apk" || { echo "Apk installation failed. Aborting."; exit 1; }
 
-    # Run the tests
-    local test_command="adb shell am instrument -w -e debug false -e client_version $CLIENT_VERSION -e service_version $SERVICE_VERSION"
-    local client_test_runner="androidx.media2.test.client.test/androidx.test.runner.AndroidJUnitRunner"
-    local service_test_runner="androidx.media2.test.service.test/androidx.test.runner.AndroidJUnitRunner"
+  echo "Running the tests"
+  local test_command="adb shell am instrument -w -e debug false -e client_version $CLIENT_VERSION -e service_version $SERVICE_VERSION"
+  local client_test_runner="androidx.media2.test.client.test/androidx.test.runner.AndroidJUnitRunner"
+  local service_test_runner="androidx.media2.test.service.test/androidx.test.runner.AndroidJUnitRunner"
 
-    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Started: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<"
+  echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Started: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<"
 
-    if [[ $OPTION_TEST_TARGET == *"client"* ]]; then
-      ${test_command} $OPTION_TEST_TARGET ${client_test_runner}
-    elif [[ $OPTION_TEST_TARGET == *"service"* ]]; then
-      ${test_command} $OPTION_TEST_TARGET ${service_test_runner}
-    else
-        ${test_command} ${client_test_runner}
-        ${test_command} ${service_test_runner}
-    fi
+  if [[ $OPTION_TEST_TARGET == *"client"* ]]; then
+    ${test_command} $OPTION_TEST_TARGET ${client_test_runner}
+  elif [[ $OPTION_TEST_TARGET == *"service"* ]]; then
+    ${test_command} $OPTION_TEST_TARGET ${service_test_runner}
+  else
+    ${test_command} ${client_test_runner}
+    ${test_command} ${service_test_runner}
+  fi
 
-    echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Ended: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<<<"
-  done
+  echo ">>>>>>>>>>>>>>>>>>>>>>>> Test Ended: Client-$CLIENT_VERSION & Service-$SERVICE_VERSION <<<<<<<<<<<<<<<<<<<<<<<<<<"
 }
 
 