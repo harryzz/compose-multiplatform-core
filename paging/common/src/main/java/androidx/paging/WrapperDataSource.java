aosp-mirror/platform_frameworks_support

ListenableFuture API Overhaul
Create new LisenableFuture<> based API underlying callback loading APIs.

 - Simplifies async initialization - PagedLists with async initial loads no longer start empty
 - Lays groundwork for cancellation
 - Enables other async APIs in the future
 - Tiled (non-contiguous) loading was removed, will be added back in a follow-up
 - RxPagedListBuilder support for new updates/signalling will be in a follow-up

Test: ./gradlew :paging:paging-common:test :paging:paging-common-ktx:test :paging:paging-runtime:cC :paging:paging-runtime-ktx:cC :paging:
paging-rxjava:test :paging:paging-rxjava-ktx:test
Fixes: 110843692

Change-Id: Ied0f1da732a1527f9fef524e37889146084acb7b

paging/common/src/main/java/androidx/paging/WrapperDataSource.java
@@ -1,0 +1,104 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.paging;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.arch.core.util.Function;
+import androidx.paging.futures.DirectExecutor;
+import androidx.paging.futures.Futures;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.IdentityHashMap;
+import java.util.List;
+
+class WrapperDataSource<K, A, B> extends DataSource<K, B> {
+    private final DataSource<K, A> mSource;
+
+    @SuppressWarnings("WeakerAccess") /* synthetic access */
+    final Function<List<A>, List<B>> mListFunction;
+
+
+    private final IdentityHashMap<B, K> mKeyMap;
+
+    WrapperDataSource(DataSource<K, A> source, Function<List<A>, List<B>> listFunction) {
+        super(source.mType);
+        mSource = source;
+        mListFunction = listFunction;
+        mKeyMap = source.mType == KeyType.ITEM_KEYED ? new IdentityHashMap<B, K>() : null;
+    }
+
+    @Override
+    public void addInvalidatedCallback(@NonNull InvalidatedCallback onInvalidatedCallback) {
+        mSource.addInvalidatedCallback(onInvalidatedCallback);
+    }
+
+    @Override
+    public void removeInvalidatedCallback(@NonNull InvalidatedCallback onInvalidatedCallback) {
+        mSource.removeInvalidatedCallback(onInvalidatedCallback);
+    }
+
+    @Override
+    public void invalidate() {
+        mSource.invalidate();
+    }
+
+    @Override
+    public boolean isInvalid() {
+        return mSource.isInvalid();
+    }
+
+    @Nullable
+    @Override
+    K getKey(@NonNull B item) {
+        if (mKeyMap != null) {
+            synchronized (mKeyMap) {
+                return mKeyMap.get(item);
+            }
+        }
+        // positional / page-keyed
+        return null;
+    }
+
+    @SuppressWarnings("WeakerAccess") /* synthetic access */
+    void stashKeysIfNeeded(@NonNull List<A> source, @NonNull List<B> dest) {
+        if (mKeyMap != null) {
+            synchronized (mKeyMap) {
+                for (int i = 0; i < dest.size(); i++) {
+                    mKeyMap.put(dest.get(i), mSource.getKey(source.get(i)));
+                }
+            }
+        }
+    }
+
+    @Override
+    final ListenableFuture<? extends BaseResult> load(@NonNull Params params) {
+        //noinspection unchecked
+        return Futures.transform(
+                mSource.load(params),
+                new Function<BaseResult<A>, BaseResult<B>>() {
+                    @Override
+                    public BaseResult<B> apply(BaseResult<A> input) {
+                        BaseResult<B> result = new BaseResult<>(input, mListFunction);
+                        stashKeysIfNeeded(input.data, result.data);
+                        return result;
+                    }
+                },
+                DirectExecutor.INSTANCE);
+    }
+}

harryzz/compose-multiplatform-core

 - Simplifies async initialization - PagedLists with async initial
   loads no longer start empty
 - No longer need to disable placeholders to disable
   PositionalDataSource tiling, due to late PagedList creation.
 - DataSource#getExecutor is pretty gross, but lets us build callback
   impls on top of Listenable*DataSource APIs without adding them to
   Params. Problem is you can't call ListenableFuture based load
   methods on callback when a PagedList isn't used without crashing
   (e.g. in tests).
 - Refresh in LivePagedListBuilder currently uses private APIs
 - Can't yet use 'Result' types with callbacks - needed once we add
   more functionality.
 - Tiled (non-contiguous) loading was removed, will be added back in a
   follow-up
 - RxPagedListBuilder support for new refresh system will be in a

paging/common/src/main/java/androidx/paging/WrapperDataSource.java
@@ -1,0 +1,110 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.paging;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.arch.core.util.Function;
+import androidx.paging.futures.DirectExecutor;
+import androidx.paging.futures.Futures;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.IdentityHashMap;
+import java.util.List;
+
+/**
+ * @param <Key> DataSource key type, same for original and wrapped.
+ * @param <ValueFrom> Value type of original DataSource.
+ * @param <ValueTo> Value type of new DataSource.
+ */
+class WrapperDataSource<Key, ValueFrom, ValueTo> extends DataSource<Key, ValueTo> {
+    private final DataSource<Key, ValueFrom> mSource;
+
+    @SuppressWarnings("WeakerAccess") /* synthetic access */
+    final Function<List<ValueFrom>, List<ValueTo>> mListFunction;
+
+
+    private final IdentityHashMap<ValueTo, Key> mKeyMap;
+
+    WrapperDataSource(@NonNull DataSource<Key, ValueFrom> source,
+            @NonNull Function<List<ValueFrom>, List<ValueTo>> listFunction) {
+        super(source.mType);
+        mSource = source;
+        mListFunction = listFunction;
+        mKeyMap = source.mType == KeyType.ITEM_KEYED ? new IdentityHashMap<ValueTo, Key>() : null;
+    }
+
+    @Override
+    public void addInvalidatedCallback(@NonNull InvalidatedCallback onInvalidatedCallback) {
+        mSource.addInvalidatedCallback(onInvalidatedCallback);
+    }
+
+    @Override
+    public void removeInvalidatedCallback(@NonNull InvalidatedCallback onInvalidatedCallback) {
+        mSource.removeInvalidatedCallback(onInvalidatedCallback);
+    }
+
+    @Override
+    public void invalidate() {
+        mSource.invalidate();
+    }
+
+    @Override
+    public boolean isInvalid() {
+        return mSource.isInvalid();
+    }
+
+    @Nullable
+    @Override
+    Key getKey(@NonNull ValueTo item) {
+        if (mKeyMap != null) {
+            synchronized (mKeyMap) {
+                return mKeyMap.get(item);
+            }
+        }
+        // positional / page-keyed
+        return null;
+    }
+
+    @SuppressWarnings("WeakerAccess") /* synthetic access */
+    void stashKeysIfNeeded(@NonNull List<ValueFrom> source, @NonNull List<ValueTo> dest) {
+        if (mKeyMap != null) {
+            synchronized (mKeyMap) {
+                for (int i = 0; i < dest.size(); i++) {
+                    mKeyMap.put(dest.get(i), mSource.getKey(source.get(i)));
+                }
+            }
+        }
+    }
+
+    @Override
+    final ListenableFuture<? extends BaseResult> load(@NonNull Params params) {
+        //noinspection unchecked
+        return Futures.transform(
+                mSource.load(params),
+                new Function<BaseResult<ValueFrom>, BaseResult<ValueTo>>() {
+                    @Override
+                    public BaseResult<ValueTo> apply(BaseResult<ValueFrom> input) {
+                        BaseResult<ValueTo> result = new BaseResult<>(input, mListFunction);
+                        stashKeysIfNeeded(input.data, result.data);
+                        return result;
+                    }
+                },
+                DirectExecutor.INSTANCE);
+    }
+}