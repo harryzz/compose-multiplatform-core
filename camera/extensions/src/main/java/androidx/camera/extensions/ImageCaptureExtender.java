aosp-mirror/platform_frameworks_support

Restructured extensions in order to fix vendor lib violation
Separated out dependencies that the OEM library needed for creating the
extensions library into extensions-stub. This includes the interfaces
and some of the convenience classes.
This solves the circular dependency issue where the OEM library was
loading classes from the application. This was a violation of Q policy
which manifested as a NoClassFoundError.

Bug: b/129418078
Test: ./gradlew camera:camera-extensions*:build, manual test on device
Change-Id: I806b924f65021d39b0cbb4737ed20b4d63865d0f

camera/extensions/src/main/java/androidx/camera/extensions/ImageCaptureExtender.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,131 +16,56 @@
 
 package androidx.camera.extensions;
 
-import android.content.Context;
-import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
-import androidx.camera.core.CameraInfoUnavailableException;
 import androidx.camera.core.CameraX;
-import androidx.camera.core.CameraX.LensFacing;
 import androidx.camera.core.CaptureBundle;
-import androidx.camera.core.CaptureProcessor;
 import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.extensions.impl.CaptureProcessorImpl;
+import androidx.camera.extensions.impl.CaptureStageImpl;
+import androidx.camera.extensions.impl.ImageCaptureExtenderImpl;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 /**
- * Provides interfaces that OEM need to implement to enable extension function.
+ * Class for using an OEM provided extension on image capture.
  */
-public abstract class ImageCaptureExtender {
-    private static final String TAG = "ImageCaptureExtender";
-    private final ImageCaptureConfig.Builder mBuilder;
-    protected ImageCaptureExtender mImpl;
+abstract class ImageCaptureExtender {
+    private ImageCaptureConfig.Builder mBuilder;
+    private ImageCaptureExtenderImpl mImpl;
 
-    public ImageCaptureExtender(ImageCaptureConfig.Builder builder) {
+    void init(ImageCaptureConfig.Builder builder, ImageCaptureExtenderImpl implementation) {
         mBuilder = builder;
+        mImpl = implementation;
     }
 
-    boolean loadImplementation(String className) {
-        try {
-            final Class<?> imageCaptureClass = Class.forName(className);
-            Constructor<?> imageCaptureConstructor =
-                    imageCaptureClass.getDeclaredConstructor(ImageCaptureConfig.Builder.class);
-            mImpl = (ImageCaptureExtender) imageCaptureConstructor.newInstance(mBuilder);
-        } catch (ClassNotFoundException
-                | NoSuchMethodException
-                | InstantiationException
-                | InvocationTargetException
-                | IllegalAccessException e) {
-            Log.e(TAG, "Failed to load image capture extension with exception: " + e);
-        }
-
-        if (mImpl == null) {
-            mImpl = new DefaultImageCaptureExtender(mBuilder);
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Indicates whether extension function can support with {@link ImageCaptureConfig.Builder}
-     *
-     * @return True if the specific extension function is supported for the camera device.
-     */
     public boolean isExtensionAvailable() {
-        LensFacing lensFacing = mBuilder.build().getLensFacing();
-        String cameraId;
-        try {
-            cameraId = CameraX.getCameraWithLensFacing(lensFacing);
-        } catch (CameraInfoUnavailableException e) {
-            throw new IllegalArgumentException(
-                    "Unable to attach to camera with LensFacing " + lensFacing, e);
-        }
-
-        Context context = CameraX.getContext();
-        CameraManager cameraManager = (CameraManager) context.getSystemService(
-                Context.CAMERA_SERVICE);
-        CameraCharacteristics cameraCharacteristics = null;
-        try {
-            cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
-        } catch (CameraAccessException e) {
-            throw new IllegalArgumentException(
-                    "Unable to retrieve info for camera with id " + cameraId + ".", e);
-        }
-
-        return isExtensionAvailable(cameraId, cameraCharacteristics);
-    }
-
-    protected boolean isExtensionAvailable(String cameraId,
-            CameraCharacteristics cameraCharacteristics) {
+        CameraX.LensFacing lensFacing = mBuilder.build().getLensFacing();
+        String cameraId = CameraUtil.getCameraId(lensFacing);
+        CameraCharacteristics cameraCharacteristics = CameraUtil.getCameraCharacteristics(cameraId);
         return mImpl.isExtensionAvailable(cameraId, cameraCharacteristics);
     }
 
-    /** Enable the extension if available. If not available then acts a no-op. */
     public void enableExtension() {
-        mImpl.enableExtension();
-    }
+        CameraX.LensFacing lensFacing = mBuilder.build().getLensFacing();
+        String cameraId = CameraUtil.getCameraId(lensFacing);
+        CameraCharacteristics cameraCharacteristics = CameraUtil.getCameraCharacteristics(cameraId);
+        mImpl.enableExtension(cameraId, cameraCharacteristics);
 
-    /**
-     * Sets necessary {@link CaptureStage} lists for the extension effect mode.
-     *
-     * <p>Sets one or more {@link CaptureStage} objects that depends on the requirement for the
-     * feature. If more than one {@link CaptureStage} is set, then the processing step must
-     * be set to process the multiple results into one final result.
-     *
-     * @param captureStages The necessary {@link CaptureStage} lists.
-     */
-    protected void setCaptureStages(@NonNull List<CaptureStage> captureStages) {
-        if (captureStages.isEmpty()) {
-            throw new IllegalArgumentException("The CaptureStage list is empty.");
+        CaptureProcessorImpl captureProcessor = mImpl.getCaptureProcessor();
+        if (captureProcessor != null) {
+            mBuilder.setCaptureProcessor(new CaptureProcessorAdaptor(captureProcessor));
         }
 
-        CaptureBundle captureBundle = new CaptureBundle();
+        List<CaptureStageImpl> captureStages = mImpl.getCaptureStages();
 
-        for (CaptureStage captureStage : captureStages) {
-            captureBundle.addCaptureStage(captureStage);
+        if (captureStages != null && !captureStages.isEmpty()) {
+            CaptureBundle captureBundle = new CaptureBundle();
+
+            for (CaptureStageImpl captureStage : captureStages) {
+                captureBundle.addCaptureStage(new CaptureStageAdaptor(captureStage));
+            }
+            mBuilder.setCaptureBundle(captureBundle);
         }
-
-        mBuilder.setCaptureBundle(captureBundle);
-    }
-
-    /**
-     * Sets the post processing step needed for the extension effect mode.
-     *
-     * <p>If there is more than one {@link CaptureStage} set by {@link #setCaptureStages(List)},
-     * then this must be set. Otherwise, this will be optional that depends on post
-     * processing requirement. The post processing will receive YUV_420_888 formatted image data.
-     * The post processing should also write out YUV_420_888 image data.
-     *
-     * @param captureProcessor The post processing implementation
-     */
-    protected void setCaptureProcessor(@NonNull CaptureProcessor captureProcessor) {
-        mBuilder.setCaptureProcessor(captureProcessor);
     }
 }

camera/extensions/src/main/java/androidx/camera/extensions/PreviewExtender.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,103 +16,64 @@
 
 package androidx.camera.extensions;
 
-import android.content.Context;
-import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.util.Log;
+import android.hardware.camera2.CaptureRequest;
+import android.util.Pair;
 
-import androidx.annotation.NonNull;
-import androidx.camera.core.CameraInfoUnavailableException;
+import androidx.camera.camera2.Camera2Config;
 import androidx.camera.core.CameraX;
-import androidx.camera.core.CameraX.LensFacing;
 import androidx.camera.core.Config;
-import androidx.camera.core.Config.Option;
 import androidx.camera.core.PreviewConfig;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import androidx.camera.extensions.impl.CaptureStageImpl;
+import androidx.camera.extensions.impl.PreviewExtenderImpl;
 
 /**
- * Provides interfaces that OEM need to implement to enable extension function in Preview.
+ * Class for using an OEM provided extension on view finder.
  */
 public abstract class PreviewExtender {
-    private static final String TAG = "PreviewExtender";
-    private final PreviewConfig.Builder mBuilder;
-    protected PreviewExtender mImpl;
+    private PreviewConfig.Builder mBuilder;
+    private PreviewExtenderImpl mImpl;
 
-    public PreviewExtender(PreviewConfig.Builder builder) {
+    void init(PreviewConfig.Builder builder, PreviewExtenderImpl implementation) {
         mBuilder = builder;
-    }
-
-    boolean loadImplementation(String className) {
-        try {
-            final Class<?> previewClass = Class.forName(className);
-            Constructor<?> previewConstructor =
-                    previewClass.getDeclaredConstructor(PreviewConfig.Builder.class);
-            mImpl = (PreviewExtender) previewConstructor.newInstance(mBuilder);
-        } catch (ClassNotFoundException
-                | NoSuchMethodException
-                | InstantiationException
-                | InvocationTargetException
-                | IllegalAccessException e) {
-            Log.e(TAG, "Failed to load view finder extension with exception: " + e);
-        }
-
-        if (mImpl == null) {
-            mImpl = new DefaultPreviewExtender(mBuilder);
-            return false;
-        }
-
-        return true;
+        mImpl = implementation;
     }
 
     /**
-     * Indicates whether extension function can support with {@link PreviewConfig.Builder}
+     * Indicates whether extension function can support with
+     * {@link PreviewConfig.Builder}
      *
      * @return True if the specific extension function is supported for the camera device.
      */
     public boolean isExtensionAvailable() {
-        LensFacing lensFacing = mBuilder.build().getLensFacing();
-        String cameraId;
-        try {
-            cameraId = CameraX.getCameraWithLensFacing(lensFacing);
-        } catch (CameraInfoUnavailableException e) {
-            throw new IllegalArgumentException(
-                    "Unable to attach to camera with LensFacing " + lensFacing, e);
-        }
-
-        Context context = CameraX.getContext();
-        CameraManager cameraManager = (CameraManager) context.getSystemService(
-                Context.CAMERA_SERVICE);
-        CameraCharacteristics cameraCharacteristics = null;
-        try {
-            cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
-        } catch (CameraAccessException e) {
-            throw new IllegalArgumentException(
-                    "Unable to retrieve info for camera with id " + cameraId + ".", e);
-        }
-
-        return isExtensionAvailable(cameraId, cameraCharacteristics);
-    }
-
-    protected boolean isExtensionAvailable(String cameraId,
-            CameraCharacteristics cameraCharacteristics) {
+        CameraX.LensFacing lensFacing = mBuilder.build().getLensFacing();
+        String cameraId = CameraUtil.getCameraId(lensFacing);
+        CameraCharacteristics cameraCharacteristics = CameraUtil.getCameraCharacteristics(cameraId);
         return mImpl.isExtensionAvailable(cameraId, cameraCharacteristics);
     }
 
-    /** Enable the extension if available. If not available then acts a no-op. */
     public void enableExtension() {
-        mImpl.enableExtension();
-    }
+        CameraX.LensFacing lensFacing = mBuilder.build().getLensFacing();
+        String cameraId = CameraUtil.getCameraId(lensFacing);
+        CameraCharacteristics cameraCharacteristics = CameraUtil.getCameraCharacteristics(cameraId);
+        mImpl.enableExtension(cameraId, cameraCharacteristics);
 
-    protected void setCaptureStage(@NonNull CaptureStage captureStage) {
-        Config config = captureStage.getCaptureConfig().getImplementationOptions();
+        CaptureStageImpl captureStage = mImpl.getCaptureStage();
 
-        for (Option<?> option : config.listOptions()) {
+        Camera2Config.Builder camera2ConfigurationBuilder =
+                new Camera2Config.Builder();
+
+        for (Pair<CaptureRequest.Key, Object> captureParameter : captureStage.getParameters()) {
+            camera2ConfigurationBuilder.setCaptureRequestOption(captureParameter.first,
+                    captureParameter.second);
+        }
+
+        Camera2Config camera2Configuration = camera2ConfigurationBuilder.build();
+
+        for (Config.Option<?> option : camera2Configuration.listOptions()) {
             @SuppressWarnings("unchecked") // Options/values are being copied directly
-                    Option<Object> objectOpt = (Option<Object>) option;
-            mBuilder.insertOption(objectOpt, config.retrieveOption(objectOpt));
+                    Config.Option<Object> objectOpt = (Config.Option<Object>) option;
+            mBuilder.insertOption(objectOpt, camera2Configuration.retrieveOption(objectOpt));
         }
     }
 }

harryzz/compose-multiplatform-core

camera/extensions/src/main/java/androidx/camera/extensions/ImageCaptureExtender.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,131 +16,56 @@
 
 package androidx.camera.extensions;
 
-import android.content.Context;
-import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
-import androidx.camera.core.CameraInfoUnavailableException;
 import androidx.camera.core.CameraX;
-import androidx.camera.core.CameraX.LensFacing;
 import androidx.camera.core.CaptureBundle;
-import androidx.camera.core.CaptureProcessor;
 import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.extensions.impl.CaptureProcessorImpl;
+import androidx.camera.extensions.impl.CaptureStageImpl;
+import androidx.camera.extensions.impl.ImageCaptureExtenderImpl;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 /**
- * Provides interfaces that OEM need to implement to enable extension function.
+ * Class for using an OEM provided extension on image capture.
  */
-public abstract class ImageCaptureExtender {
-    private static final String TAG = "ImageCaptureExtender";
-    private final ImageCaptureConfig.Builder mBuilder;
-    protected ImageCaptureExtender mImpl;
+abstract class ImageCaptureExtender {
+    private ImageCaptureConfig.Builder mBuilder;
+    private ImageCaptureExtenderImpl mImpl;
 
-    public ImageCaptureExtender(ImageCaptureConfig.Builder builder) {
+    void init(ImageCaptureConfig.Builder builder, ImageCaptureExtenderImpl implementation) {
         mBuilder = builder;
+        mImpl = implementation;
     }
 
-    boolean loadImplementation(String className) {
-        try {
-            final Class<?> imageCaptureClass = Class.forName(className);
-            Constructor<?> imageCaptureConstructor =
-                    imageCaptureClass.getDeclaredConstructor(ImageCaptureConfig.Builder.class);
-            mImpl = (ImageCaptureExtender) imageCaptureConstructor.newInstance(mBuilder);
-        } catch (ClassNotFoundException
-                | NoSuchMethodException
-                | InstantiationException
-                | InvocationTargetException
-                | IllegalAccessException e) {
-            Log.e(TAG, "Failed to load image capture extension with exception: " + e);
-        }
-
-        if (mImpl == null) {
-            mImpl = new DefaultImageCaptureExtender(mBuilder);
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Indicates whether extension function can support with {@link ImageCaptureConfig.Builder}
-     *
-     * @return True if the specific extension function is supported for the camera device.
-     */
     public boolean isExtensionAvailable() {
-        LensFacing lensFacing = mBuilder.build().getLensFacing();
-        String cameraId;
-        try {
-            cameraId = CameraX.getCameraWithLensFacing(lensFacing);
-        } catch (CameraInfoUnavailableException e) {
-            throw new IllegalArgumentException(
-                    "Unable to attach to camera with LensFacing " + lensFacing, e);
-        }
-
-        Context context = CameraX.getContext();
-        CameraManager cameraManager = (CameraManager) context.getSystemService(
-                Context.CAMERA_SERVICE);
-        CameraCharacteristics cameraCharacteristics = null;
-        try {
-            cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
-        } catch (CameraAccessException e) {
-            throw new IllegalArgumentException(
-                    "Unable to retrieve info for camera with id " + cameraId + ".", e);
-        }
-
-        return isExtensionAvailable(cameraId, cameraCharacteristics);
-    }
-
-    protected boolean isExtensionAvailable(String cameraId,
-            CameraCharacteristics cameraCharacteristics) {
+        CameraX.LensFacing lensFacing = mBuilder.build().getLensFacing();
+        String cameraId = CameraUtil.getCameraId(lensFacing);
+        CameraCharacteristics cameraCharacteristics = CameraUtil.getCameraCharacteristics(cameraId);
         return mImpl.isExtensionAvailable(cameraId, cameraCharacteristics);
     }
 
-    /** Enable the extension if available. If not available then acts a no-op. */
     public void enableExtension() {
-        mImpl.enableExtension();
-    }
+        CameraX.LensFacing lensFacing = mBuilder.build().getLensFacing();
+        String cameraId = CameraUtil.getCameraId(lensFacing);
+        CameraCharacteristics cameraCharacteristics = CameraUtil.getCameraCharacteristics(cameraId);
+        mImpl.enableExtension(cameraId, cameraCharacteristics);
 
-    /**
-     * Sets necessary {@link CaptureStage} lists for the extension effect mode.
-     *
-     * <p>Sets one or more {@link CaptureStage} objects that depends on the requirement for the
-     * feature. If more than one {@link CaptureStage} is set, then the processing step must
-     * be set to process the multiple results into one final result.
-     *
-     * @param captureStages The necessary {@link CaptureStage} lists.
-     */
-    protected void setCaptureStages(@NonNull List<CaptureStage> captureStages) {
-        if (captureStages.isEmpty()) {
-            throw new IllegalArgumentException("The CaptureStage list is empty.");
+        CaptureProcessorImpl captureProcessor = mImpl.getCaptureProcessor();
+        if (captureProcessor != null) {
+            mBuilder.setCaptureProcessor(new AdaptingCaptureProcessor(captureProcessor));
         }
 
-        CaptureBundle captureBundle = new CaptureBundle();
+        List<CaptureStageImpl> captureStages = mImpl.getCaptureStages();
 
-        for (CaptureStage captureStage : captureStages) {
-            captureBundle.addCaptureStage(captureStage);
+        if (captureStages != null && !captureStages.isEmpty()) {
+            CaptureBundle captureBundle = new CaptureBundle();
+
+            for (CaptureStageImpl captureStage : captureStages) {
+                captureBundle.addCaptureStage(new AdaptingCaptureStage(captureStage));
+            }
+            mBuilder.setCaptureBundle(captureBundle);
         }
-
-        mBuilder.setCaptureBundle(captureBundle);
-    }
-
-    /**
-     * Sets the post processing step needed for the extension effect mode.
-     *
-     * <p>If there is more than one {@link CaptureStage} set by {@link #setCaptureStages(List)},
-     * then this must be set. Otherwise, this will be optional that depends on post
-     * processing requirement. The post processing will receive YUV_420_888 formatted image data.
-     * The post processing should also write out YUV_420_888 image data.
-     *
-     * @param captureProcessor The post processing implementation
-     */
-    protected void setCaptureProcessor(@NonNull CaptureProcessor captureProcessor) {
-        mBuilder.setCaptureProcessor(captureProcessor);
     }
 }

camera/extensions/src/main/java/androidx/camera/extensions/PreviewExtender.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,103 +16,65 @@
 
 package androidx.camera.extensions;
 
-import android.content.Context;
-import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.util.Log;
+import android.hardware.camera2.CaptureRequest;
+import android.util.Pair;
 
-import androidx.annotation.NonNull;
-import androidx.camera.core.CameraInfoUnavailableException;
+import androidx.camera.camera2.Camera2Config;
 import androidx.camera.core.CameraX;
-import androidx.camera.core.CameraX.LensFacing;
 import androidx.camera.core.Config;
-import androidx.camera.core.Config.Option;
 import androidx.camera.core.PreviewConfig;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import androidx.camera.extensions.impl.CaptureStageImpl;
+import androidx.camera.extensions.impl.PreviewExtenderImpl;
 
 /**
- * Provides interfaces that OEM need to implement to enable extension function in Preview.
+ * Class for using an OEM provided extension on view finder.
  */
 public abstract class PreviewExtender {
-    private static final String TAG = "PreviewExtender";
-    private final PreviewConfig.Builder mBuilder;
-    protected PreviewExtender mImpl;
+    private PreviewConfig.Builder mBuilder;
+    private PreviewExtenderImpl mImpl;
 
-    public PreviewExtender(PreviewConfig.Builder builder) {
+    void init(PreviewConfig.Builder builder, PreviewExtenderImpl implementation) {
         mBuilder = builder;
-    }
-
-    boolean loadImplementation(String className) {
-        try {
-            final Class<?> previewClass = Class.forName(className);
-            Constructor<?> previewConstructor =
-                    previewClass.getDeclaredConstructor(PreviewConfig.Builder.class);
-            mImpl = (PreviewExtender) previewConstructor.newInstance(mBuilder);
-        } catch (ClassNotFoundException
-                | NoSuchMethodException
-                | InstantiationException
-                | InvocationTargetException
-                | IllegalAccessException e) {
-            Log.e(TAG, "Failed to load view finder extension with exception: " + e);
-        }
-
-        if (mImpl == null) {
-            mImpl = new DefaultPreviewExtender(mBuilder);
-            return false;
-        }
-
-        return true;
+        mImpl = implementation;
     }
 
     /**
-     * Indicates whether extension function can support with {@link PreviewConfig.Builder}
+     * Indicates whether extension function can support with
+     * {@link PreviewConfig.Builder}
      *
      * @return True if the specific extension function is supported for the camera device.
      */
     public boolean isExtensionAvailable() {
-        LensFacing lensFacing = mBuilder.build().getLensFacing();
-        String cameraId;
-        try {
-            cameraId = CameraX.getCameraWithLensFacing(lensFacing);
-        } catch (CameraInfoUnavailableException e) {
-            throw new IllegalArgumentException(
-                    "Unable to attach to camera with LensFacing " + lensFacing, e);
-        }
-
-        Context context = CameraX.getContext();
-        CameraManager cameraManager = (CameraManager) context.getSystemService(
-                Context.CAMERA_SERVICE);
-        CameraCharacteristics cameraCharacteristics = null;
-        try {
-            cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
-        } catch (CameraAccessException e) {
-            throw new IllegalArgumentException(
-                    "Unable to retrieve info for camera with id " + cameraId + ".", e);
-        }
-
-        return isExtensionAvailable(cameraId, cameraCharacteristics);
-    }
-
-    protected boolean isExtensionAvailable(String cameraId,
-            CameraCharacteristics cameraCharacteristics) {
+        CameraX.LensFacing lensFacing = mBuilder.build().getLensFacing();
+        String cameraId = CameraUtil.getCameraId(lensFacing);
+        CameraCharacteristics cameraCharacteristics = CameraUtil.getCameraCharacteristics(cameraId);
         return mImpl.isExtensionAvailable(cameraId, cameraCharacteristics);
     }
 
-    /** Enable the extension if available. If not available then acts a no-op. */
     public void enableExtension() {
-        mImpl.enableExtension();
-    }
+        CameraX.LensFacing lensFacing = mBuilder.build().getLensFacing();
+        String cameraId = CameraUtil.getCameraId(lensFacing);
+        CameraCharacteristics cameraCharacteristics = CameraUtil.getCameraCharacteristics(cameraId);
+        mImpl.enableExtension(cameraId, cameraCharacteristics);
 
-    protected void setCaptureStage(@NonNull CaptureStage captureStage) {
-        Config config = captureStage.getCaptureConfig().getImplementationOptions();
+        CaptureStageImpl captureStage = mImpl.getCaptureStage();
 
-        for (Option<?> option : config.listOptions()) {
+        Camera2Config.Builder camera2ConfigurationBuilder =
+                new Camera2Config.Builder();
+
+        for (Pair<CaptureRequest.Key, Object> captureParameter : captureStage.getParameters()) {
+            camera2ConfigurationBuilder.setCaptureRequestOption(captureParameter.first,
+                    captureParameter.second);
+        }
+
+        Camera2Config camera2Config = camera2ConfigurationBuilder.build();
+
+        for (Config.Option<?> option : camera2Config.listOptions()) {
             @SuppressWarnings("unchecked") // Options/values are being copied directly
-                    Option<Object> objectOpt = (Option<Object>) option;
-            mBuilder.getMutableConfig().insertOption(objectOpt, config.retrieveOption(objectOpt));
+                    Config.Option<Object> objectOpt = (Config.Option<Object>) option;
+            mBuilder.getMutableConfig().insertOption(objectOpt,
+                    camera2Config.retrieveOption(objectOpt));
         }
     }
 }