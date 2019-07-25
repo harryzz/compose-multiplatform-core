aosp-mirror/platform_frameworks_support

Reveal TrustedWebActivityService classes.
Change-Id: I56c2c89a3d48a1e27e6fb6ffa794d368251bc58f
Test: ./gradlew browser:assemble && ./gradlew browser:checkApi

buildSrc/src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -33,7 +33,7 @@ object LibraryVersions {
     val AUTOFILL = Version("1.0.0-alpha02")
     val BENCHMARK = Version("1.0.0-alpha04")
     val BIOMETRIC = Version("1.0.0-alpha05")
-    val BROWSER = Version("1.2.0-alpha04")
+    val BROWSER = Version("1.2.0-alpha05")
     val CAMERA = Version("1.0.0-alpha04")
     val CAR = Version("1.0.0-alpha8")
     val CAR_CLUSTER = Version("1.0.0-alpha6")

harryzz/compose-multiplatform-core

buildSrc/src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -33,7 +33,7 @@ object LibraryVersions {
     val AUTOFILL = Version("1.0.0-alpha02")
     val BENCHMARK = Version("1.0.0-alpha04")
     val BIOMETRIC = Version("1.0.0-beta01")
-    val BROWSER = Version("1.2.0-alpha06")
+    val BROWSER = Version("1.2.0-alpha07")
     val CAMERA = Version("1.0.0-alpha04")
     val CAMERA_EXTENSIONS = Version("1.0.0-alpha01")
     val CAMERA_VIEW = Version("1.0.0-alpha01")