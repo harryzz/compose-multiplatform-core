69dd3a6e02528960fb9211a5202b9a0335e88451
Alexander Maryanovsky <msasha@gmail.com>
2024-12-19 13:58:23 +0200

    Fix previous commit, setting `firstDownRefersToPrimaryMouseButtonOnly() = true`.

--- compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/gestures/TapGestureDetector.skiko.kt
+++ compose/foundation/foundation/src/skikoMain/kotlin/androidx/compose/foundation/gestures/TapGestureDetector.skiko.kt
@@ -16,11 +16,9 @@
 
 package androidx.compose.foundation.gestures
 
-import androidx.compose.foundation.implementedInJetBrainsFork
 import androidx.compose.ui.input.pointer.PointerEvent
 
 actual val PointerEvent.isDeepPress: Boolean
     get() = false // TODO: Support PointerEvent.isDeepPress
 
-internal actual fun firstDownRefersToPrimaryMouseButtonOnly(): Boolean =
-    implementedInJetBrainsFork()
+internal actual fun firstDownRefersToPrimaryMouseButtonOnly(): Boolean = true
