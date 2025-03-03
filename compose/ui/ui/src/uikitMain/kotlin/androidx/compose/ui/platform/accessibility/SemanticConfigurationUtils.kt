276cccfaa6d964c45ca480d2f0883b1a26a4f743
Andrei Salavei <Andrei.Salavei@jetbrains.com>
2025-03-03 15:17:15 +0100

    Enable text input for UI Tests and Accessibility (#1875)
    Use `TextView` accessibility traits bitmask for text fields.
    Mark text input as `Editing` when text input is focused.
    
    Fixes
    https://youtrack.jetbrains.com/issue/CMP-7222/Support-accessibility-text-input
    
    ### Features - iOS
    - Support accessibility text input
    - Support text input for UI Tests

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/accessibility/SemanticConfigurationUtils.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/accessibility/SemanticConfigurationUtils.kt
@@ -40,7 +40,7 @@ import platform.UIKit.UIAccessibilityTraitUpdatesFrequently
 import platform.UIKit.UIAccessibilityTraits
 
 // Private accessibility trait for text fields
-internal val CMPAccessibilityTraitTextField: UIAccessibilityTraits = 1UL shl 18
+internal val CMPAccessibilityTraitTextView: UIAccessibilityTraits = (1UL shl 18) or (1UL shl 47)
 internal val CMPAccessibilityTraitIsEditing: UIAccessibilityTraits = 1UL shl 21
 
 internal fun SemanticsConfiguration.accessibilityTraits(): UIAccessibilityTraits {
@@ -86,10 +86,11 @@ internal fun SemanticsConfiguration.accessibilityTraits(): UIAccessibilityTraits
         }
     }
 
-    if (contains(SemanticsProperties.EditableText) &&
-        contains(SemanticsActions.SetText)
-    ) {
-        result = result or CMPAccessibilityTraitTextField
+    if (contains(SemanticsProperties.EditableText)) {
+        result = result or CMPAccessibilityTraitTextView
+        if (getOrNull(SemanticsProperties.Focused) == true) {
+            result = result or CMPAccessibilityTraitIsEditing
+        }
     } else if (contains(SemanticsActions.OnClick)) {
         result = result or UIAccessibilityTraitButton
     }
