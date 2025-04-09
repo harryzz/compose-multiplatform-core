2ad2ee7eb9a398d27cee256890d76199a1ab2ce8
Andrei Salavei <Andrei.Salavei@jetbrains.com>
2025-04-09 09:27:16 +0200

    Throttle accessibility tree reload when focus changes (#1986)
    Fixes
    https://youtrack.jetbrains.com/issue/CMP-7947/Investigate-A11y-issues-in-KotlinConf-app
    
    ## Release Notes
    ### Fixes - iOS
    - Fixes an issue where the accessibility engine could leave a scrollable
    list without reading it to the end

--- compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/Accessibility.uikit.kt
+++ compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/Accessibility.uikit.kt
@@ -742,20 +742,7 @@ internal class AccessibilityMediator(
             field = value
             accessibilityDebugLogger?.log("Focus mode: $focusMode")
 
-            val ids = (focusMode as? AccessibilityElementFocusMode.KeepFocus)?.key?.let {
-                accessibilityElementsMap[it]?.node?.semanticsNode?.allScrollableParentNodeIds
-            } ?: emptySet()
-
-            if (focusedNodesScrollableParentsIds != ids) {
-                focusedNodesScrollableParentsIds = ids
-                invalidationChannel.trySend(Unit)
-
-                if (ids.isNotEmpty()) {
-                    // Hack to fix an issue where iOS accessibility only reads the items visible
-                    // at the moment of the beginning of the "Speak Screen" command.
-                    UIAccessibilityPostNotification(UIAccessibilityPageScrolledNotification, null)
-                }
-            }
+            scheduleFocusedScrollableParentsIdsUpdate()
         }
 
     var focusedNodesScrollableParentsIds = setOf<Int>()
@@ -951,6 +938,30 @@ internal class AccessibilityMediator(
 
         root.element = null
         accessibilityElementsMap.clear()
+    }
+
+    private var focusedScrollableParentsIdsUpdateJob: Job? = null
+    private fun scheduleFocusedScrollableParentsIdsUpdate() {
+        focusedScrollableParentsIdsUpdateJob?.cancel()
+        focusedScrollableParentsIdsUpdateJob = coroutineScope.launch {
+            // Throttle the recalculation of scrollable parent node IDs to avoid unnecessary
+            // reloading of the accessibility tree when the focusMode changes quickly.
+            delay(10)
+            val ids = (focusMode as? AccessibilityElementFocusMode.KeepFocus)?.key?.let {
+                accessibilityElementsMap[it]?.node?.semanticsNode?.allScrollableParentNodeIds
+            } ?: emptySet()
+
+            if (focusedNodesScrollableParentsIds != ids) {
+                focusedNodesScrollableParentsIds = ids
+                invalidationChannel.trySend(Unit)
+
+                if (ids.isNotEmpty()) {
+                    // Hack to fix an issue where iOS accessibility only reads the items visible
+                    // at the moment of the beginning of the "Speak Screen" command.
+                    UIAccessibilityPostNotification(UIAccessibilityPageScrolledNotification, null)
+                }
+            }
+        }
     }
 
     private fun createOrUpdateAccessibilityElement(
