6d3b49efd7db03860f94251e62d4d5798b83d821
Igor Demin <igor.demin@jetbrains.com>
2025-04-11 01:40:56 +0200

    git checkout --no-overlay v1.7.3 -- compose/material3/material3-common

--- compose/material3/material3-common/src/wasmJsMain/kotlin/androidx/compose/material3/common/internal/IdentityHashCode.wasmJs.kt
+++ compose/material3/material3-common/src/wasmJsMain/kotlin/androidx/compose/material3/common/internal/IdentityHashCode.wasmJs.kt
@@ -16,31 +16,9 @@
 
 package androidx.compose.material3.common.internal
 
-import androidx.compose.runtime.NoLiveLiterals
-
-private var nextHash = 1
-
-private external interface WeakMap {
-    fun set(key: JsAny, value: Int)
-    fun get(key: JsAny): Int?
-}
-
-private val weakMap: WeakMap = js("new WeakMap()")
-@NoLiveLiterals
-private fun memoizeIdentityHashCode(instance: JsAny): Int {
-    val value = nextHash++
-
-    weakMap.set(instance.toJsReference(), value)
-
-    return value
-}
-
-// For the reference check the identityHashCode in compose:runtime
 internal actual fun identityHashCode(instance: Any?): Int {
     if (instance == null) {
         return 0
     }
-
-    val jsRef = instance.toJsReference()
-    return weakMap.get(jsRef) ?: memoizeIdentityHashCode(jsRef)
+    return instance.hashCode()
 }
