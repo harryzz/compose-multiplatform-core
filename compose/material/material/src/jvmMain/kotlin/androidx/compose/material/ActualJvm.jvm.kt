eb8473d77169c6acc1726ed5d0a391c10e108683
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-04-30 19:47:53 +0200

    Revert material atomic diff

--- compose/material/material/src/jvmMain/kotlin/androidx/compose/material/ActualJvm.jvm.kt
+++ compose/material/material/src/jvmMain/kotlin/androidx/compose/material/ActualJvm.jvm.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 The Android Open Source Project
+ * Copyright 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,16 +14,7 @@
  * limitations under the License.
  */
 
-package androidx.compose.material.internal
+package androidx.compose.material
 
-import kotlinx.atomicfu.atomic
-
-internal actual class AtomicReference<V> actual constructor(value: V) {
-    private val delegate = atomic(value)
-    actual fun get() = delegate.value
-    actual fun set(value: V) {
-        delegate.value = value
-    }
-    actual fun getAndSet(value: V) = delegate.getAndSet(value)
-    actual fun compareAndSet(expect: V, newValue: V) = delegate.compareAndSet(expect, newValue)
-}
+internal actual typealias InternalAtomicReference<V> =
+    java.util.concurrent.atomic.AtomicReference<V>
